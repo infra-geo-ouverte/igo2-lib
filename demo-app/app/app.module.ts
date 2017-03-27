28a9b143d3123113a5d7f0a597e9cc618f7c1ff3
cbourget <cbourget@mapgears.com>
2017-03-27 16:59:34 -0400

    fix(search source): remove test search source

--- demo-app/app/app.module.ts
+++ demo-app/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, Http } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { IgoModule, provideDefaultSearchSources } from '../../lib';
--- lib/search/search-sources/index.ts
+++ lib/search/search-sources/index.ts
@@ -1,3 +1,2 @@
 export * from './search-source';
 export * from './search-source-nominatim';
-export * from './search-source-icherche';
