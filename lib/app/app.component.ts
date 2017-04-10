438f88d679d13228d0f37f5a5968bc0da5735b30
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-04-10 11:16:20 -0400

    test(*): clean test files

--- lib/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'igo-lib',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-}
--- lib/test.main.ts
+++ lib/test.main.ts
@@ -1,11 +1,10 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { TestModule } from './test.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(TestModule);
