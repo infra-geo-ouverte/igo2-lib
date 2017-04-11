b021a1a74fb47f0407eb1f2e506e07d1a33c2212
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-04-11 15:14:17 -0400

    Update main.ts

--- lib/test/main.ts
+++ lib/test/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from '../environments/environment';
 import { IgoTestModule } from './module';
 
 if (environment.production) {
