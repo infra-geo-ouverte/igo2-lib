eaea180ccdfeb3bab0da8f4a2275dee1f070d6ff
cbourget <charlesericbourget@gmail.com>
2017-04-25 16:18:34 -0400

    feat(sidenav): shim directive to prevent the default focus behavior

---
+++ src/lib/shared/sidenav/sidenav-shim.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SidenavShimDirective } from './sidenav-shim.directive';
+
+describe('SidenavShimDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SidenavShimDirective();
+    expect(directive).toBeTruthy();
+  });
+});
