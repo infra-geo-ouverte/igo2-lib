63a07b7142facd927f26fce7d5c28c7c62190ec1
cbourget <charlesericbourget@gmail.com>
2017-04-25 16:18:34 -0400

    feat(sidenav): shim directive to prevent the default focus behavior

---
+++ src/lib/shared/sidenav/sidenav-shim.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, Self, HostListener, Renderer } from '@angular/core';
+import { MdSidenav } from '@angular/material';
+
+
+/**
+ * <igoSidenavShim> directive.
+ *
+ * This directive prevents a material sidenav with mode="side"
+ * from focusing an element after it's closed
+ */
+@Directive({
+  selector: '[igoSidenavShim]'
+})
+export class SidenavShimDirective {
+
+  private focusedElement: HTMLElement;
+  private blurElement: HTMLElement;
+
+  @HostListener('open', ['$event']) onOpen() {
+    this.focusedElement = document.activeElement as HTMLElement;
+  }
+
+  @HostListener('close-start', ['$event']) onCloseStart() {
+    const focusedElement = document.activeElement as HTMLElement;
+    if (focusedElement !== this.focusedElement) {
+      this.blurElement = this.focusedElement;
+    } else {
+      this.blurElement = undefined;
+    }
+  }
+
+  @HostListener('close', ['$event']) onClose() {
+    if (this.blurElement) {
+      this.renderer.invokeElementMethod(this.blurElement, 'blur');
+    }
+
+    this.blurElement = undefined;
+    this.focusedElement = undefined;
+  }
+
+  constructor(@Self() component: MdSidenav,
+              private renderer: Renderer) {}
+
+}
