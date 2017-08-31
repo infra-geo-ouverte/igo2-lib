40089528c42ce5a9a43154e900e917e58176196d
Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
2017-08-31 15:18:49 -0400

    update angular to complete the tests

--- src/lib/customMaterialModule/CustomMaterialModule.ts
+++ src/lib/customMaterialModule/CustomMaterialModule.ts
@@ -3,32 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { A11yModule } from '@angular/material';
 import { CompatibilityModule } from '@angular/material';
-import { MdAutocompleteModule } from '@angular/material'; 
+import { MdAutocompleteModule } from '@angular/material';
 import { MdButtonToggleModule } from '@angular/material';
 import { MdButtonModule } from '@angular/material';
 import { MdCheckboxModule } from '@angular/material';
-import { MdDialogModule } from '@angular/material';  
-import { MdListModule } from '@angular/material';  
+import { MdDialogModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
 import { MdGridListModule } from '@angular/material';
-import { MdCardModule } from '@angular/material';  
-import { MdChipsModule } from '@angular/material';  
-import { MdIconModule } from '@angular/material';  
-import { MdInputModule } from '@angular/material'; 
-import { MdMenuModule } from '@angular/material';  
+import { MdCardModule } from '@angular/material';
+import { MdChipsModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
 import { MdProgressSpinnerModule } from '@angular/material';
-import { MdProgressBarModule } from '@angular/material'; 
-import { MdRadioModule } from '@angular/material'; 
+import { MdProgressBarModule } from '@angular/material';
+import { MdRadioModule } from '@angular/material';
 import { MdRippleModule } from '@angular/material';
-import { MdSelectModule } from '@angular/material';  
+import { MdSelectModule } from '@angular/material';
 import { MdSlideToggleModule } from '@angular/material';
-import { MdSliderModule } from '@angular/material';  
-import { MdSidenavModule } from '@angular/material'; 
+import { MdSliderModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
 import { MdSnackBarModule } from '@angular/material';
-import { MdTabsModule } from '@angular/material';  
-import { MdToolbarModule } from '@angular/material';  
+import { MdTabsModule } from '@angular/material';
+import { MdToolbarModule } from '@angular/material';
 import { MdTooltipModule } from '@angular/material';
-import { OverlayModule } from '@angular/material';
-import { PlatformModule } from '@angular/material'; 
 import { StyleModule } from '@angular/material';
 import { MdTableModule } from '@angular/material';
 
@@ -49,7 +47,7 @@ const MATERIAL_MODULES = [
   MdSidenavModule,
   MdTabsModule,
   MdToolbarModule,
-
+  MdTableModule,
   // These modules include providers.
   A11yModule,
   CompatibilityModule,
@@ -62,11 +60,7 @@ const MATERIAL_MODULES = [
   MdSlideToggleModule,
   MdSnackBarModule,
   MdTooltipModule,
-  OverlayModule,
-  PlatformModule,
-  StyleModule,
-  
-  MdTableModule
+  StyleModule
 ];
 
 
