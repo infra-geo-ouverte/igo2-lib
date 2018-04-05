7c1773c0032be7838410e8d5b3680297643af6cd
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-04-05 11:06:27 -0400

    feat(message): can pass templateRef to message service

--- src/lib/core/message/shared/message.interface.ts
+++ src/lib/core/message/shared/message.interface.ts
@@ -1,9 +1,10 @@
+import { TemplateRef } from '@angular/core';
 import { MessageType } from './message.enum';
 
 export interface Message {
   title?: string;
   text?: string;
-  html?: string;
+  html?: string | TemplateRef<any>;
   icon?: string;
   type?: MessageType;
   options?: any;
