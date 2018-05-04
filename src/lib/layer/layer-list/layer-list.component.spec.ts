96758bcca781126fb0558071236527ec6c109239
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-05-04 15:38:08 -0400

    test(filter): fix test ogc-filter

--- src/lib/layer/layer-list/layer-list.component.spec.ts
+++ src/lib/layer/layer-list/layer-list.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IgoTestModule } from '../../../test/module';
 import { IgoSharedModule } from '../../shared';
+import { IgoFilterModule } from '../../filter';
 
 import { LayerListComponent } from './layer-list.component';
 import { LayerItemComponent } from '../layer-item/layer-item.component';
@@ -11,20 +12,18 @@ describe('LayerListComponent', () => {
   let component: LayerListComponent;
   let fixture: ComponentFixture<LayerListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        IgoTestModule,
-        IgoSharedModule
-      ],
-      declarations: [
-        LayerListComponent,
-        LayerItemComponent,
-        LayerLegendComponent
-      ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [IgoTestModule, IgoSharedModule, IgoFilterModule],
+        declarations: [
+          LayerListComponent,
+          LayerItemComponent,
+          LayerLegendComponent
+        ]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LayerListComponent);
