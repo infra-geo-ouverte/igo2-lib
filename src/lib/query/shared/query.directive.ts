81254be46bd1bd15314484ec5aa10d0628d6a10e
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2018-05-14 16:09:15 -0400

    Make features clickable

--- src/lib/query/shared/query.directive.ts
+++ src/lib/query/shared/query.directive.ts
@@ -9,9 +9,9 @@ import { IgoMap } from '../../map/shared';
 import { MapBrowserComponent } from '../../map/map-browser';
 import { Layer } from '../../layer';
 import { Feature } from '../../feature';
-
+import * as ol from 'openlayers';
 import { QueryService } from '../shared/query.service';
-
+import { LanguageService } from '../../core';
 
 @Directive({
   selector: '[igoQuery]'
@@ -39,7 +39,8 @@ export class QueryDirective implements AfterViewInit, OnDestroy {
   }>();
 
   constructor(@Self() private component: MapBrowserComponent,
-              private queryService: QueryService) {}
+              private queryService: QueryService,
+              private languageService: LanguageService) {}
 
   ngAfterViewInit() {
     this.queryLayers$$ = this.component.map.layers$
@@ -68,25 +69,71 @@ export class QueryDirective implements AfterViewInit, OnDestroy {
   private handleMapClick(event: ol.MapBrowserEvent) {
     this.unsubscribeQueries();
 
+    const clickedFeatures: ol.Feature[] = []
+    const format = new ol.format.GeoJSON();
+    const mapProjection = this.map.projection;
+    this.map.ol.forEachFeatureAtPixel(event.pixel,
+       function(feature: ol.Feature, layer: ol.layer.Layer) {
+         if (layer.getZIndex() !== 999) {
+           let title;
+           if (layer.get('title') !== undefined) {
+             title = layer.get('title')
+            } else {
+              title = this.map.layers.filter((f) => f['zIndex'] === layer.getZIndex())[0]
+              .dataSource['options']['title']
+            }
+          feature.set('clickedTitle', title)
+          clickedFeatures.push(feature)
+         }
+       }.bind(this));
+       const featuresGeoJSON = JSON.parse(
+         format.writeFeatures(clickedFeatures, {
+           dataProjection: 'EPSG:4326',
+           featureProjection: mapProjection
+          })
+        );
+  let i = 0;
+  let parsedClickedFeatures: Feature[] = [];
+  parsedClickedFeatures = featuresGeoJSON.features.map(f =>
+    Object.assign({}, f, {
+      source: this.languageService.translate.instant('igo.clickOnMap.clickedFeature'),
+      id: f.properties.clickedTitle + ' ' + String(i++),
+      icon: 'mouse',
+      title: f.properties.clickedTitle
+    })
+  );
+  parsedClickedFeatures.forEach(element => {
+    delete element.properties['clickedTitle'];
+  });
     const view = this.map.ol.getView();
     const queries$ = this.queryService.query(this.queryLayers, {
       coordinates: event.coordinate,
       projection: this.map.projection,
       resolution: view.getResolution()
     });
-
-    if (this.waitForAllQueries) {
-      this.queries$$.push(
-        forkJoin(...queries$).subscribe(
-          (features: Feature[][]) => this.query.emit({features: features, event: event})
-        )
-      );
+    if (queries$.length === 0) {
+      this.query.emit({features: parsedClickedFeatures, event: event})
     } else {
-      this.queries$$ = queries$.map((query$: Observable<Feature[]>) => {
-        return query$.subscribe(
-          (features: Feature[]) => this.query.emit({features: features, event: event})
+      if (this.waitForAllQueries) {
+        this.queries$$.push(
+          forkJoin(...queries$).subscribe(
+            (features: Feature[][]) =>
+            this.query.emit({
+              features: features.filter((f) => f.length > 0).concat(parsedClickedFeatures),
+              event: event
+            })
+          )
         );
-      });
+      } else {
+        this.queries$$ = queries$.map((query$: Observable<Feature[]>) => {
+          return query$.subscribe(
+            (features: Feature[]) => this.query.emit({
+              features: parsedClickedFeatures.concat(features),
+              event: event
+            })
+          );
+        });
+      }
     }
   }
 
