95f3acb1406ecc24f1b8d20af20f5fa0db4e6249
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-05-18 16:29:16 -0400

    remove env

--- src/demo-app/environments/environment.ts
+++ src/demo-app/environments/environment.ts
@@ -8,31 +8,18 @@ import { IgoEnvironment } from '../../lib';
 interface Environment {
   production: boolean;
   igo: IgoEnvironment;
-}
+};
 
 export const environment: Environment = {
   production: false,
   igo: {
     searchSources: {
       icherche: {
-        enabled: true,
-        locateUrl: '/icherche/xy'
-      },
-      datasource: {
         enabled: false
       }
     },
     language: {
       prefix: './assets/locale/'
-    },
-    auth: {
-      url: 'http://localhost:8000/users',
-      tokenKey: 'igo_token',
-      google: {
-        apiKey: 'AIzaSyCbc-E35ZNqAjPvpbr30bAXwfcQoq5XLBs',
-        clientId:
-          '467961599657-f7lebhfn3oposibnrvlgjl7ffglgr2go.apps.googleusercontent.com'
-      }
     }
   }
 };
