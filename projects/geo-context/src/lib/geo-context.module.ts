1b91d38f3a61ce63c812e134c550288ac3a93f10
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-08-14 17:10:55 -0400

    feat(sidenav): add sidenav component

--- projects/geo-context/src/lib/geo-context.module.ts
+++ projects/geo-context/src/lib/geo-context.module.ts
@@ -1,13 +1,21 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { IgoContextModule } from './context/context.module';
+import { IgoContextMapButtonModule } from './context-map-button/context-map-button.module';
 import { IgoShareMapModule } from './share-map/share-map.module';
+import { IgoSidenavModule } from './sidenav/sidenav.module';
 import { IgoToolModule } from './tool/tool.module';
 
 @NgModule({
   imports: [],
   declarations: [],
-  exports: [IgoContextModule, IgoShareMapModule, IgoToolModule]
+  exports: [
+    IgoContextModule,
+    IgoContextMapButtonModule,
+    IgoShareMapModule,
+    IgoSidenavModule,
+    IgoToolModule
+  ]
 })
 export class IgoGeoContextModule {
   static forRoot(): ModuleWithProviders {
