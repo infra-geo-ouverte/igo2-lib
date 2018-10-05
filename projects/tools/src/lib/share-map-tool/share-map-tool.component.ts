a632017356843b6d21d8f3e8f2890ece48135144
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-10-05 13:44:51 -0400

    fix(context): fix default context

--- projects/tools/src/lib/share-map-tool/share-map-tool.component.ts
+++ projects/tools/src/lib/share-map-tool/share-map-tool.component.ts
@@ -24,7 +24,7 @@ export class ShareMapToolComponent {
 
   get hasShareMapButton(): boolean {
     return this.options.hasShareMapButton === undefined
-      ? false
+      ? true
       : this.options.hasShareMapButton;
   }
 
