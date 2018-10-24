402d21c5831adda014d6759b1975b1dae38ad635
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-10-24 13:55:17 -0400

    fix(dropGeoFile): directive not working (build prod - aot)

--- projects/geo/src/lib/import-export/shared/drop-geo-file.directive.ts
+++ projects/geo/src/lib/import-export/shared/drop-geo-file.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, EventEmitter } from '@angular/core';
+import { Directive, EventEmitter, HostListener } from '@angular/core';
 
 import { DragAndDropDirective } from '@igo2/common';
 import { ImportExportService } from './import-export.service';
@@ -16,5 +16,20 @@ export class DropGeoFileDirective extends DragAndDropDirective {
     this.allowed_extensions = ['zip', 'geojson', 'kml', 'gml', 'json'];
     this.filesDropped.subscribe(f => importExportService.import(f));
     this.filesInvalid.subscribe(f => importExportService.onFilesInvalid(f));
+  }
+
+  @HostListener('dragover', ['$event'])
+  public onDragOver(evt) {
+    super.onDragOver(evt);
+  }
+
+  @HostListener('dragleave', ['$event'])
+  public onDragLeave(evt) {
+    super.onDragLeave(evt);
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop(evt) {
+    super.onDrop(evt);
   }
 }
