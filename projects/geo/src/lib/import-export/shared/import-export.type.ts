9591f763b3282cc0fb3352b5ef98aee2b93fb18b
gignacnic <gignacnic@gmail.com>
2019-02-01 16:37:23 -0500

    add(import-export): gpx support format

--- projects/geo/src/lib/import-export/shared/import-export.type.ts
+++ projects/geo/src/lib/import-export/shared/import-export.type.ts
@@ -1,4 +1,4 @@
 import { strEnum } from '@igo2/utils';
 
-export const ExportFormat = strEnum(['GeoJSON', 'KML', 'GML', 'shapefile']);
+export const ExportFormat = strEnum(['GeoJSON', 'KML', 'GPX', 'GML', 'shapefile']);
 export type ExportFormat = keyof typeof ExportFormat;
