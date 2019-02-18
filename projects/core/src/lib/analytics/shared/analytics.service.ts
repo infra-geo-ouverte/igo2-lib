8d73e28ef278c1d8c1bdb6332e29395648367230
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-02-18 15:14:33 -0500

    fix(analytics): change piwik to matomo

--- projects/core/src/lib/analytics/shared/analytics.service.ts
+++ projects/core/src/lib/analytics/shared/analytics.service.ts
@@ -27,14 +27,14 @@ export class AnalyticsService {
     window['_paq'].push(['trackPageView']);
     window['_paq'].push(['enableLinkTracking']);
     (() => {
-      window['_paq'].push(['setTrackerUrl', this.options.url + 'piwik.php']);
+      window['_paq'].push(['setTrackerUrl', this.options.url + 'matomo.php']);
       window['_paq'].push(['setSiteId', this.options.id]);
       const g = document.createElement('script');
       const s = document.getElementsByTagName('script')[0];
       g.type = 'text/javascript';
       g.async = true;
       g.defer = true;
-      g.src = this.options.url + 'piwik.js';
+      g.src = this.options.url + 'matomo.js';
       s.parentNode.insertBefore(g, s);
     })();
   }
