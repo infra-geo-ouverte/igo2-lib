d3a8f32012a12fb5ef8ef4a75dc805023784c750
cbourget <charlesericbourget@gmail.com>
2019-03-01 10:01:54 -0500

    feat(projection): inject the projection service with the map state

--- packages/geo/src/lib/map/shared/index.ts
+++ packages/geo/src/lib/map/shared/index.ts
@@ -2,3 +2,5 @@ export * from './map';
 export * from './map.interface';
 export * from './map.service';
 export * from './map.utils';
+export * from './projection.interfaces';
+export * from './projection.service';
--- packages/integration/src/lib/map/map.state.ts
+++ packages/integration/src/lib/map/map.state.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { IgoMap, MapService } from '@igo2/geo';
+import { IgoMap, MapService, ProjectionService } from '@igo2/geo';
 
 /**
  * Service that holds the state of the map module
@@ -17,7 +17,10 @@ export class MapState {
   }
   private _map: IgoMap;
 
-  constructor(private mapService: MapService) {
+  constructor(
+    private mapService: MapService,
+    private projectionService: ProjectionService
+  ) {
     this._map = new IgoMap({
       controls: {
         scaleLine: true,
