403e748fd091bffd333cae74f9d3cf75056f7096
cbourget <charlesericbourget@gmail.com>
2019-05-10 13:59:22 -0400

    feat(measure): improve the measure dialog interface

--- packages/geo/src/lib/measure/measurer/measurer-dialog.component.ts
+++ packages/geo/src/lib/measure/measurer/measurer-dialog.component.ts
@@ -3,12 +3,18 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 import { MeasurerDialogData } from '../shared/measure.interfaces';
 
+import { MeasureAreaUnit, MeasureLengthUnit} from '../shared/measure.enum';
+
 @Component({
   selector: 'igo-measurer-dialog',
   templateUrl: 'measurer-dialog.component.html',
   styleUrls: ['./measurer-dialog.component.scss']
 })
 export class MeasurerDialogComponent {
+
+  measureAreaUnit = MeasureAreaUnit;
+
+  measureLengthUnit = MeasureLengthUnit;
 
   constructor(
     public dialogRef: MatDialogRef<MeasurerDialogComponent>,
