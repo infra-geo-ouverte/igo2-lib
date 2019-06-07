1787f028edbf9bb36126b31cf56e4432814df809
Déreck Lynch <50959537+drekss@users.noreply.github.com>
2019-06-07 12:26:02 -0400

    fix(mvt): add onLayerStatusChange (#347)
    * feat(datasource,layer): add MVT(MapBox Vector Tile) and Vector Tile layer supported as new datasource
    
    * fix(mvt): add onLayerStatusChange
    
    * fix(lint) in mvt-datasource and vectortile-layer

--- projects/geo/src/lib/datasource/shared/datasources/mvt-datasource.interface.ts
+++ projects/geo/src/lib/datasource/shared/datasources/mvt-datasource.interface.ts
@@ -4,7 +4,7 @@ import olAttribution from 'ol/control/Attribution';
 import { DataSourceOptions } from './datasource.interface';
 
 export interface MVTDataSourceOptions extends DataSourceOptions {
-  // type?: 'mvt'; 
+  // type?: 'mvt';
   projection?: string;
   attributions?: olAttribution;
   format?: any;
--- projects/geo/src/lib/datasource/shared/datasources/mvt-datasource.ts
+++ projects/geo/src/lib/datasource/shared/datasources/mvt-datasource.ts
@@ -3,7 +3,7 @@ import feature from 'ol/Feature';
 import olSourceVectorTile from 'ol/source/VectorTile';
 import olFormatMVT from 'ol/format/MVT';
 
-import { uuid } from '@igo2/utils';
+import { uuid, SubjectStatus } from '@igo2/utils';
 
 import { DataSource } from './datasource';
 import { MVTDataSourceOptions } from './mvt-datasource.interface';
@@ -15,16 +15,16 @@ export class MVTDataSource extends DataSource {
   protected createOlSource(): olSourceVectorTile {
     const mvtFormat = new olFormatMVT({featureClass: feature});
     this.options.format = mvtFormat;
-    
     return new olSourceVectorTile(this.options);
   }
 
   protected generateId() {
-    if(!this.options.url){
+    if (!this.options.url) {
         return uuid();
     }
     const chain = 'mvt' + this.options.url;
     return Md5.hashStr(chain) as string;
-    
   }
+
+  onLayerStatusChange(status: SubjectStatus): void {}
 }
