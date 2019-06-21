d3944e1a60fbd65efc5542c65f78e80907aff0a9
PE Lord <pe_lord@yahoo.ca>
2019-06-21 08:40:48 -0400

    refactor(query.component) Wrong urls

--- demo/src/app/geo/query/query.component.ts
+++ demo/src/app/geo/query/query.component.ts
@@ -64,8 +64,7 @@ export class AppQueryComponent {
     this.dataSourceService
       .createAsyncDataSource({
         type: 'wms',
-        // url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
-        url: '/swtq',
+        url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
         queryable: true,
         params: {
           layers: 'bgr_v_sous_route_res_sup_act',
@@ -84,8 +83,7 @@ export class AppQueryComponent {
     this.dataSourceService
       .createAsyncDataSource({
         type: 'wms',
-        // url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
-        url: '/swtq',
+        url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
         queryable: true,
         queryFormat: QueryFormat.HTMLGML2,
         queryHtmlTarget: QueryHtmlTarget.IFRAME,
