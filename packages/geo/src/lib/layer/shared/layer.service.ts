f8b4549d3b2b2f0ce1db919beacaab8ea47baefe
dereck.lynch <dereck.lynch47@hotmail.com>
2019-06-28 09:20:17 -0400

    feat(datasource,layer): add MVT(MapBox Vector Tile) and Vector Tile layer supported as new datasource

--- packages/geo/src/lib/layer/shared/layer.service.ts
+++ packages/geo/src/lib/layer/shared/layer.service.ts
@@ -15,7 +15,8 @@ import {
   CartoDataSource,
   ArcGISRestDataSource,
   TileArcGISRestDataSource,
-  WebSocketDataSource
+  WebSocketDataSource,
+  MVTDataSource
 } from '../../datasource';
 
 import { DataSourceService } from '../../datasource/shared/datasource.service';
@@ -28,7 +29,9 @@ import {
   TileLayerOptions,
   VectorLayer,
   VectorLayerOptions,
-  AnyLayerOptions
+  AnyLayerOptions,
+  VectorTileLayer,
+  VectorTileLayerOptions
 } from './layers';
 
 import { StyleService } from './style.service';
@@ -82,6 +85,9 @@ export class LayerService {
       case WMSDataSource:
         layer = this.createImageLayer(layerOptions as ImageLayerOptions);
         break;
+      case MVTDataSource:
+        layer = this.createVectorTileLayer(layerOptions as VectorTileLayerOptions);
+        break;
       default:
         break;
     }
@@ -132,5 +138,16 @@ export class LayerService {
     });
 
     return new VectorLayer(layerOptionsOl);
+  }
+
+  private createVectorTileLayer(layerOptions: VectorTileLayerOptions): VectorTileLayer {
+    let style;
+    if (layerOptions.style !== undefined) {
+      style = this.styleService.createStyle(layerOptions.style);
+    }
+    const layerOptionsOl = Object.assign({}, layerOptions, {
+      style
+    });
+    return new VectorTileLayer(layerOptionsOl);
   }
 }
