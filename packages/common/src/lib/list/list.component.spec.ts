c5bc986451ac22c6dd1486d9bc3d75ee471ecd93
Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
2019-07-30 08:20:06 -0400

    test(geo.search.search-settings) fix test

--- packages/common/src/lib/list/list.component.spec.ts
+++ packages/common/src/lib/list/list.component.spec.ts
@@ -1,25 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IgoLanguageModule } from '@igo2/core';
+import { SearchSettingsComponent } from './search-settings.component';
+import { CommonModule } from '@angular/common';
+import {
+  MatTooltipModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatCheckboxModule
+} from '@angular/material';
 
-import { MatListModule } from '@angular/material';
+import { SearchSourceService } from '../shared/search-source.service';
+import { provideDefaultIChercheSearchResultFormatter } from '../shared/sources/icherche.providers';
+import { provideDefaultCoordinatesSearchResultFormatter } from '../shared/sources/coordinates.providers';
+import { SearchSource } from '../shared/sources/source';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-import { ListComponent } from './list.component';
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
-describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+describe('SearchSettingsComponent', () => {
+  let component: SearchSettingsComponent;
+  let fixture: ComponentFixture<SearchSettingsComponent>;
 
   beforeEach(async(() => {
+
+    const spy = jasmine.createSpyObj('SearchSourceService', ['getSources']);
+
     TestBed.configureTestingModule({
       imports: [
-        MatListModule
-      ],
-      declarations: [ ListComponent ]
+        HttpClientModule,
+        TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useFactory: HttpLoaderFactory,
+                        deps: [HttpClient]
+                    }
+                }),
+        IgoLanguageModule,
+        CommonModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatCheckboxModule],
+      declarations: [ SearchSettingsComponent ],
+      providers: [
+        { provide: SearchSourceService, useValue: spy },
+        provideDefaultIChercheSearchResultFormatter(),
+        provideDefaultCoordinatesSearchResultFormatter()
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
+    fixture = TestBed.createComponent(SearchSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
