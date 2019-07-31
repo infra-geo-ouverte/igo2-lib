08ef2eccd774e68a1c858603941f59940daae7e8
Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
2019-07-31 16:13:33 -0400

    feat(vector-layer-watcher) add watcher to vector layer

--- demo/src/app/geo/layer/layer.component.ts
+++ demo/src/app/geo/layer/layer.component.ts
@@ -49,114 +49,26 @@ export class AppLayerComponent {
         );
       });
 
-    interface WFSoptions
-      extends WFSDataSourceOptions,
-        OgcFilterableDataSourceOptions {}
-
-    const wfsDatasource: WFSoptions = {
-      type: 'wfs',
-      url: 'https://geoegl.msp.gouv.qc.ca/apis/ws/igo_gouvouvert.fcgi',
-      params: {
-        featureTypes: 'vg_observation_v_autre_wmst',
-        fieldNameGeometry: 'geometry',
-        maxFeatures: 10000,
-        version: '2.0.0',
-        outputFormat: 'geojson_utf8',
-        outputFormatDownload: 'shp'
-      },
-      ogcFilters: {
-        enabled: true,
-        editable: true,
-        filters: {
-          operator: 'PropertyIsEqualTo',
-          propertyName: 'code_municipalite',
-          expression: '10043'
-        }
-      }
-    };
-
-    this.dataSourceService
-      .createAsyncDataSource(wfsDatasource)
-      .subscribe(dataSource => {
-        const layer: LayerOptions = {
-          title: 'WFS ',
-          visible: true,
-          source: dataSource
-        };
-        this.map.addLayer(this.layerService.createLayer(layer));
-      });
 
     this.layerService
       .createAsyncLayer({
+        title: "Point temps réel",
+        visible: true,
         sourceOptions: {
-          type: 'wms',
-          url: 'https://geoegl.msp.gouv.qc.ca/apis/ws/igo_gouvouvert.fcgi',
-          optionsFromCapabilities: true,
-          params: {
-            layers: 'MELS_CS_ANGLO_S',
-            version: '1.3.0'
-          }
+          type: "websocket",
+          url: "wss://testgeoegl.msp.gouv.qc.ca/apis/gps/",
+          formatOptions: {
+                  dataProjection: "EPSG:4326",
+                  featureProjection: "EPSG:3857"
+                },
+          onmessage: "delete"
+        },
+          animation: {
+          duration: 3000
         }
+
       })
       .subscribe(l => this.map.addLayer(l));
 
-    this.layerService
-      .createAsyncLayer({
-        title: 'Réseau routier',
-        visible: false,
-        sourceOptions: {
-          type: 'wms',
-          url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
-          params: {
-            layers: 'bgr_v_sous_route_res_sup_act',
-            version: '1.3.0'
-          }
-        }
-      })
-      .subscribe(l => this.map.addLayer(l));
-
-    this.layerService
-      .createAsyncLayer({
-        title: 'Avertissements routier',
-        visible: false,
-        sourceOptions: {
-          type: 'wms',
-          url: 'https://ws.mapserver.transports.gouv.qc.ca/swtq',
-          params: {
-            layers: 'evenements',
-            version: '1.3.0'
-          }
-        }
-      })
-      .subscribe(l => this.map.addLayer(l));
-
-    const datasource: WMSDataSourceOptions = {
-      type: 'wms',
-      url: 'https://geoegl.msp.gouv.qc.ca/apis/ws/igo_gouvouvert.fcgi',
-      refreshIntervalSec: 15,
-      params: {
-        layers: 'vg_observation_v_inondation_embacle_wmst',
-        version: '1.3.0'
-      }
-    };
-
-    interface LayerOptionsWithMetadata
-      extends LayerOptions,
-        MetadataLayerOptions {}
-
-    this.dataSourceService
-      .createAsyncDataSource(datasource)
-      .subscribe(dataSource => {
-        const layer: LayerOptionsWithMetadata = {
-          title: 'Embâcle',
-          source: dataSource,
-          metadata: {
-            url:
-              'https://www.donneesquebec.ca/recherche/fr/dataset/historique-publique-d-embacles-repertories-au-msp',
-            extern: true
-          }
-        };
-        this.map.addLayer(this.layerService.createLayer(layer));
-      });
   }
 }
