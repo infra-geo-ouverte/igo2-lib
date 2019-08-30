fae073ea668451c27fe1ab06c9d4100cc49cca31
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-08-30 12:26:47 -0400

    Update about-tool.component.ts

--- packages/integration/src/lib/about/about-tool/about-tool.component.ts
+++ packages/integration/src/lib/about/about-tool/about-tool.component.ts
@@ -12,19 +12,6 @@ import { ToolComponent } from '@igo2/common';
   templateUrl: './about-tool.component.html'
 })
 export class AboutToolComponent {
-  // @Input() html: string = `
-  //   <h1>About IGO</h1>
-  //   <p>IGO (for Open GIS Infrastructure) is a Free Open Source Software
-  //   for Geospatial (FOSS4G) developed by organisations in the government
-  //   of Quebec in Canada. The objective is to make it open, common,
-  //   modular, based on open governance model supported by multiple
-  //   organisations. IGO is a Web GIS software with a client & server
-  //   component to manage and publish massive amount of Geospatial data.
-  //   </p>
-  //   </hr>
-  //   <a href='mailto:info@igouverte.org' target='_top'>info[@]igouverte.org</a>
-  //   </br>
-  //   <a href='http://www.igouverte.org' target='_blank'>www.igouverte.org</A>`;
 
   @Input()
   get html() {
