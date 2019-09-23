30d07dd0538b7a72370fa68182e2c8b120c5d652
Déreck Lynch <50959537+drekss@users.noreply.github.com>
2019-09-23 14:02:10 -0400

    feat(network): now works with cordova (#393)

--- packages/core/src/lib/network/network.service.ts
+++ packages/core/src/lib/network/network.service.ts
@@ -2,6 +2,9 @@ import { Injectable, EventEmitter, OnDestroy, Injector } from '@angular/core';
 import { Observable, Subscription, fromEvent } from 'rxjs';
 import { debounceTime, startWith } from 'rxjs/operators';
 
+import { Network } from '@ionic-native/network/ngx';
+import { Platform } from '@ionic/angular';
+
 import { MessageService } from '../message/shared/message.service';
 import { LanguageService } from '../language/shared/language.service';
 
@@ -17,6 +20,7 @@ export class NetworkService implements OnDestroy {
   private stateChangeEventEmitter = new EventEmitter<ConnectionState>();
   private onlineSubscription: Subscription;
   private offlineSubscription: Subscription;
+  private connectionType: string;
 
   private state: ConnectionState = {
     connection: window.navigator.onLine
@@ -24,10 +28,22 @@ export class NetworkService implements OnDestroy {
 
   constructor(
     private messageService: MessageService,
-    private injector: Injector
+    private injector: Injector,
+    private network: Network,
+    private platform: Platform
     ) {
-    this.checkNetworkState();
-  }
+      this.checkNetworkState();
+      this.platform.ready().then(() => {
+        if (this.platform.is('cordova')) {
+          if (this.platform.is('android')) {
+            this.checkNetworkStateMobile();
+          }
+        } else {
+          console.log('browser');
+          this.checkNetworkState();
+        }
+      });
+    }
 
   private checkNetworkState() {
     this.onlineSubscription = fromEvent(window, 'online').subscribe(() => {
@@ -46,6 +62,41 @@ export class NetworkService implements OnDestroy {
       this.messageService.info(message, title);
       this.state.connection = false;
       this.emitEvent();
+    });
+  }
+
+  private checkNetworkStateMobile() {
+    if (this.network.type !== this.network.Connection.NONE) {
+      this.connectionType = this.network.type;
+      this.state.connection = true;
+    }
+
+    this.offlineSubscription = this.network.onDisconnect().subscribe(() => {
+      this.state.connection = false;
+      setTimeout(() => {
+        if (!this.state.connection) {
+          const translate = this.injector.get(LanguageService).translate;
+          const message = translate.instant('igo.core.network.offline.message');
+          const title = translate.instant('igo.core.network.offline.title');
+          this.messageService.info(message, title);
+          this.state.connection = false;
+          this.emitEvent();
+        }
+      }, 10000);
+    });
+
+    this.onlineSubscription = this.network.onConnect().subscribe(() => {
+      this.state.connection = true;
+      setTimeout(() => {
+        if (this.state.connection) {
+          const translate = this.injector.get(LanguageService).translate;
+          const message = translate.instant('igo.core.network.online.message');
+          const title = translate.instant('igo.core.network.online.title');
+          this.messageService.info(message, title);
+          this.state.connection = true;
+          this.emitEvent();
+        }
+      }, 10000);
     });
   }
 
