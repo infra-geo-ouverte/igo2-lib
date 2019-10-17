6c3ab5a567f09a31342d82e31a13542c0cf7d538
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2019-10-17 14:10:14 -0400

    Delete 3-release.bat

--- 3-release.bat
+++
@@ -1,9 +0,0 @@
-call npm run build.geo
-
-REM robocopy dist\auth    ..\igo2\node_modules\@igo2\auth /COPYALL /E
-REM robocopy dist\common  ..\igo2\node_modules\@igo2\common /COPYALL /E
-REM robocopy dist\context ..\igo2\node_modules\@igo2\context /COPYALL /E
-REM robocopy dist\core    ..\igo2\node_modules\@igo2\core /COPYALL /E
-robocopy dist\geo     ..\igo2\node_modules\@igo2\geo /COPYALL /E
-REM robocopy dist\integration   ..\igo2\node_modules\@igo2\integration /COPYALL /E
-REM robocopy dist\utils    ..\igo2\node_modules\@igo2\utils /COPYALL /E
