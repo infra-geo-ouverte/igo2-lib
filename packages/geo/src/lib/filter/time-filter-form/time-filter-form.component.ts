0716bc289564df1a2f87f6ed585631503815fbb7
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2019-10-21 11:45:53 -0400

    fix(time-filter )Stop filter animation on disable

--- packages/geo/src/lib/filter/time-filter-form/time-filter-form.component.ts
+++ packages/geo/src/lib/filter/time-filter-form/time-filter-form.component.ts
@@ -273,6 +273,7 @@ export class TimeFilterFormComponent implements OnInit {
         this.yearChange.emit(this.year);
       }
     } else {
+      this.stopFilter();
       this.storeCurrentFilterValue();
       this.change.emit(undefined); // TODO: FIX THIS for ALL OTHER TYPES STYLES OR RANGE.
     }
