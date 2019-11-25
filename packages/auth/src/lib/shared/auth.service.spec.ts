f2a0e9eb9a5fa6eb8422d9ed0f1bc3a9c1051094
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-11-25 07:23:56 -0500

    Update auth.service.spec.ts

--- packages/auth/src/lib/shared/auth.service.spec.ts
+++ packages/auth/src/lib/shared/auth.service.spec.ts
@@ -1,22 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
-<<<<<<< HEAD
 import { IgoLanguageModule, IgoMessageModule } from '@igo2/core';
-=======
-import { IgoMessageModule, IgoLanguageModule } from '@igo2/core';
->>>>>>> spatial filter tool
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD
       imports: [HttpClientModule, IgoLanguageModule, IgoMessageModule],
-=======
-      imports: [HttpClientModule, IgoMessageModule, IgoLanguageModule],
->>>>>>> spatial filter tool
       providers: [AuthService]
     });
   });
