a157337bee56c35c54e6ae45c1d60f199cd8c8fa
Déreck Lynch <50959537+drekss@users.noreply.github.com>
2019-12-17 13:50:09 -0500

    feat(feature): possibility to generate mvt data source as regular features or as renderFeatures. (#527)
    * feat(feature): possibility to generate mvt data source as regular features or as renderFeatures.
    
    * fix missing code
    
    * removed useless conditions

--- packages/geo/src/lib/query/shared/query.directive.ts
+++ packages/geo/src/lib/query/shared/query.directive.ts
@@ -12,6 +12,7 @@ import {
 import { Subscription, Observable, of, zip } from 'rxjs';
 
 import OlFeature from 'ol/Feature';
+import OlRenderFeature from 'ol/render/Feature';
 import OlLayer from 'ol/layer/Layer';
 
 import OlDragBoxInteraction from 'ol/interaction/DragBox';
@@ -21,6 +22,7 @@ import { ListenerFunction } from 'ol/events';
 import { IgoMap } from '../../map/shared/map';
 import { MapBrowserComponent } from '../../map/map-browser/map-browser.component';
 import { Feature } from '../../feature/shared/feature.interfaces';
+import { renderFeatureFromOl } from '../../feature/shared/feature.utils';
 import { featureFromOl } from '../../feature/shared/feature.utils';
 import { QueryService } from './query.service';
 import { layerIsQueryable, olLayerIsQueryable } from './query.utils';
@@ -200,8 +202,20 @@ export class QueryDirective implements AfterViewInit, OnDestroy {
               };
               clickedFeatures.push(newFeature);
             }
+          } else if (featureOL instanceof OlRenderFeature) {
+            const featureFromRender: OlFeature = featureOL;
+            const feature = renderFeatureFromOl(
+              featureOL,
+              this.map.projection,
+              layerOL
+            );
+            clickedFeatures.push(feature);
           } else {
-            const feature = featureFromOl(featureOL, this.map.projection);
+            const feature = featureFromOl(
+              featureOL,
+              this.map.projection,
+              layerOL
+            );
             clickedFeatures.push(feature);
           }
         }
