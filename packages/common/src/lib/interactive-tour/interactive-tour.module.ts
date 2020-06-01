65a9621a6f7565e4bcb0c758583d5f9bc8d1829b
PhilippeL <PHILIPPE.LAFRENIERE@msp.gouv.qc.ca>
2020-06-01 10:18:47 -0400

    WIP

--- packages/common/src/lib/interactive-tour/interactive-tour.module.ts
+++ packages/common/src/lib/interactive-tour/interactive-tour.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InteractiveTourService } from './interactive-tour.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [InteractiveTourService]
 })
 export class InteractiveTourModule { }
