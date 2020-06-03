7c9625fe2730c313abdb0fae65d05133360847f0
Déreck Lynch <50959537+drekss@users.noreply.github.com>
2020-06-03 16:13:57 -0400

    fix(context-manager): LayerContextDirective supports all income routes (#650)
    * add params to clusterStyle
    
    * share-map cerf fixed
    
    Co-authored-by: drekss <bovictor123>

--- packages/context/src/lib/context-manager/shared/layer-context.directive.ts
+++ packages/context/src/lib/context-manager/shared/layer-context.directive.ts
@@ -49,10 +49,11 @@ export class LayerContextDirective implements OnInit, OnDestroy {
       this.route.options.contextKey
     ) {
       const queryParams$$ = this.route.queryParams
-        .pipe(skip(1))
         .subscribe(params => {
-          this.queryParams = params;
-          queryParams$$.unsubscribe();
+          if ( Object.keys(params).length > 0 ) {
+            this.queryParams = params;
+            queryParams$$.unsubscribe();
+          }
         });
     }
   }
