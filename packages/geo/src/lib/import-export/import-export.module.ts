f1af5ed8d5393c0b8021cdc563c9919a5aedd82c
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2020-06-09 11:46:28 -0400

    feat(importExport): new export method by URL (based on download property) (#661)
    * fix(importExport)Skip while exportOptions undefined
    
    * refactor(importExport) typo
    
    * feat(import-export) formats is now a observable
    
    * feat(import  export) Warning on no exportable layers available
    
    * feat(importExport) New export method bu URL (based on download property)
    
    * feat(import-export) manage downloadable format
    
    * refactor(locale) typo
    
    * feat(import-export)layer list is now observable
    
    * refactor(import-export) delay on url openning
    
    * refactor(import-export) clear the export format form if not contained in the format list
    
    * feat(import-export) try to preload features for not loaded layer/empty layers
    
    * feat(*) export button for exportable layers
    
    * feat(map) moving from download button to export button

--- packages/geo/src/lib/import-export/import-export.module.ts
+++ packages/geo/src/lib/import-export/import-export.module.ts
@@ -8,18 +8,23 @@ import {
   MatOptionModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatIconModule,
+  MatTooltipModule
 } from '@angular/material';
 
 import { IgoLanguageModule } from '@igo2/core';
 import { IgoKeyValueModule, IgoDrapDropModule, IgoSpinnerModule } from '@igo2/common';
 
+import { ExportButtonComponent } from './export-button/export-button.component';
 import { ImportExportComponent } from './import-export/import-export.component';
 import { DropGeoFileDirective } from './shared/drop-geo-file.directive';
 import { IgoStyleListModule } from './style-list/style-list.module';
 
 @NgModule({
   imports: [
+    MatIconModule,
+    MatTooltipModule,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
@@ -36,8 +41,8 @@ import { IgoStyleListModule } from './style-list/style-list.module';
     IgoDrapDropModule,
     IgoStyleListModule.forRoot()
   ],
-  exports: [ImportExportComponent, DropGeoFileDirective, IgoStyleListModule],
-  declarations: [ImportExportComponent, DropGeoFileDirective]
+  exports: [ImportExportComponent, DropGeoFileDirective, IgoStyleListModule, ExportButtonComponent],
+  declarations: [ImportExportComponent, DropGeoFileDirective, ExportButtonComponent]
 })
 export class IgoImportExportModule {
   static forRoot(): ModuleWithProviders {
