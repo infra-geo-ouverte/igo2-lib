227db73b476f08f830e7d037b9e99c5f453cdbb6
PE Lord <pe_lord@pm.me>
2020-06-10 10:17:38 -0400

    feat(workspace) creation of a workspace button

--- packages/geo/src/lib/workspace/workspace.module.ts
+++ packages/geo/src/lib/workspace/workspace.module.ts
@@ -6,19 +6,27 @@ import { provideOgcFilterWidget } from './widgets/widgets';
 
 import { IgoOgcFilterModule } from './widgets/ogc-filter/ogc-filter.module';
 import { IgoWorkspaceSelectorModule } from './workspace-selector/workspace-selector.module';
+import { MatIconModule, MatButtonModule, MatTooltipModule } from '@angular/material';
+import { WorkspaceButtonComponent } from './workspace-button/workspace-button.component';
+import { IgoLanguageModule } from '@igo2/core';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
     IgoWidgetModule,
     IgoWorkspaceSelectorModule,
-    IgoOgcFilterModule
+    IgoOgcFilterModule,
+    IgoLanguageModule
   ],
   exports: [
     IgoWorkspaceSelectorModule,
-    IgoOgcFilterModule
+    IgoOgcFilterModule,
+    WorkspaceButtonComponent
   ],
-  declarations: [],
+  declarations: [WorkspaceButtonComponent],
   providers: [
     provideOgcFilterWidget()
   ]
