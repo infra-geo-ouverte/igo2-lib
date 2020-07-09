1390f0a9cc54a0643822b8a730fb391ddec8a531
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2020-07-09 14:38:50 -0400

    update gesture

--- packages/core/src/lib/gesture/gesture.module.ts
+++ packages/core/src/lib/gesture/gesture.module.ts
@@ -1,23 +1,23 @@
-// import { NgModule, ModuleWithProviders } from '@angular/core';
-// import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-//
-// import { IgoGestureConfig } from './gesture.provider';
-//
-// @NgModule({
-//   imports: [],
-//   declarations: [],
-//   exports: []
-// })
-// export class IgoGestureModule {
-//   static forRoot(): ModuleWithProviders<IgoGestureModule> {
-//     return {
-//       ngModule: IgoGestureModule,
-//       providers: [
-//         {
-//           provide: HAMMER_GESTURE_CONFIG,
-//           useClass: IgoGestureConfig
-//         }
-//       ]
-//     };
-//   }
-// }
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HammerModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+import { IgoGestureConfig } from './gesture.provider';
+
+@NgModule({
+  imports: [HammerModule],
+  declarations: [],
+  exports: []
+})
+export class IgoGestureModule {
+  static forRoot(): ModuleWithProviders<IgoGestureModule> {
+    return {
+      ngModule: IgoGestureModule,
+      providers: [
+        {
+          provide: HAMMER_GESTURE_CONFIG,
+          useClass: IgoGestureConfig
+        }
+      ]
+    };
+  }
+}
--- packages/core/src/lib/gesture/gesture.provider.ts
+++ packages/core/src/lib/gesture/gesture.provider.ts
@@ -1,11 +1,12 @@
-// import { Injectable } from '@angular/core';
-// import { HammerManager } from '@angular/material/core';
-//
-// @Injectable()
-// export class IgoGestureConfig extends GestureConfig {
-//   buildHammer(element: HTMLElement) {
-//     const mc = super.buildHammer(element) as HammerManager;
-//     mc.set({ touchAction: 'pan-y' });
-//     return mc;
-//   }
-// }
+import { Injectable } from '@angular/core';
+import { HammerGestureConfig } from '@angular/platform-browser';
+
+
+@Injectable()
+export class IgoGestureConfig extends HammerGestureConfig {
+  buildHammer(element: HTMLElement) {
+    const mc = super.buildHammer(element) as any;
+    mc.set({ touchAction: 'pan-y' });
+    return mc;
+  }
+}
