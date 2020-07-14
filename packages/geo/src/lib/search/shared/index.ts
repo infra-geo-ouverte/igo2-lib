b3d49d4f4625dbbd23dfbec099d02592ac587045
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2020-07-14 15:01:15 -0400

    fix import

--- packages/geo/src/lib/search/shared/index.ts
+++ packages/geo/src/lib/search/shared/index.ts
@@ -3,5 +3,6 @@ export * from './search.interfaces';
 export * from './search-pointer-summary.directive';
 export * from './search.service';
 export * from './search-source.service';
+export * from './search-source-service.providers';
 export * from './search.utils';
 export * from './sources';
