e4135ed0834d82d89d7ae824a6d8dceea05c149d
PE Lord <pe_lord@pm.me>
2020-09-01 16:18:54 -0400

    feat(integration) new state for storage

---
+++ packages/integration/src/lib/storage/storage.state.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { StorageService } from '@igo2/core';
+
+/**
+ * Service that holds the state of storage service
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageState {
+  get storageService(): StorageService {
+    return this.igoStorageService;
+  }
+
+  constructor(private igoStorageService: StorageService) {}
+}
