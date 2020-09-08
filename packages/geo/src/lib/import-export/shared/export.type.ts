244416db1472f4f7effe2004cfb8a7551fdbdb3c
Philippe Lafrenière <53181414+PhilippeLafreniere18@users.noreply.github.com>
2020-09-08 16:05:25 -0400

    fix(export): fix encoding ex;l and google maps name link (#712)
    * feat(libs): upgrade librairies (#696)
    
    * chore(utils): bowser version
    
    * fix(googleMaps): Fix google maps name link
    
    * WIP
    
    * feat(libs): upgrade librairies (#696)
    
    * WIP
    
    * fix(export): Latin-1 encoding and lint
    
    * fix
    
    Co-authored-by: Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
    Co-authored-by: Marc-André Barbeau <marc-andre.barbeau@gmail.com>

--- packages/geo/src/lib/import-export/shared/export.type.ts
+++ packages/geo/src/lib/import-export/shared/export.type.ts
@@ -2,3 +2,6 @@ import { strEnum } from '@igo2/utils';
 
 export const ExportFormat = strEnum(['GeoJSON', 'GML', 'GPX', 'KML', 'Shapefile', 'CSVcomma', 'CSVsemicolon', 'URL']);
 export type ExportFormat = keyof typeof ExportFormat;
+
+export const EncodingFormat = strEnum(['UTF8', 'LATIN1']);
+export type EncodingFormat = keyof typeof EncodingFormat;
