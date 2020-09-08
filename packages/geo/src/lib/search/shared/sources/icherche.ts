e32f102b0dcd8ef8d2183f86a7d88b4c47c1e10d
Philippe Lafrenière <53181414+PhilippeLafreniere18@users.noreply.github.com>
2020-09-08 16:45:57 -0400

    feat(feature-details): add routing directive to access route from directions tool (#711)
    * feat(feature-details): add routing directive to access route from directions tool
    
    * feat(feature-details): add routing option for feature-details
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * lint
    
    * Update directions-form.component.ts
    
    Co-authored-by: Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>

--- packages/geo/src/lib/search/shared/sources/icherche.ts
+++ packages/geo/src/lib/search/shared/sources/icherche.ts
@@ -417,7 +417,7 @@ export class IChercheSearchSource extends SearchSource implements TextSearch {
   private computeProperties(data: IChercheData): { [key: string]: any } {
     const properties = ObjectUtils.removeKeys(
       data.properties,
-      IChercheSearchSource.propertiesBlacklist
+      IChercheSearchSource.propertiesBlacklist,
     );
 
     if (data.geometry === undefined) {
@@ -486,10 +486,17 @@ export class IChercheSearchSource extends SearchSource implements TextSearch {
       );
     }
 
+    const routing: {
+      Route: string
+    } = {
+      Route: '<span class="routing"> <u>' + this.languageService.translate.instant('igo.geo.seeRouting') + '</u> </span>'
+    };
+
     return Object.assign(
       { type: data.index },
       properties,
-      googleLinksProperties
+      googleLinksProperties,
+      routing
     );
   }
 
