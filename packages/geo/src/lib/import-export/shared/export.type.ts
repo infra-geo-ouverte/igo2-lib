5b3a5b7e81ea3fa4c085c1b02e55a4ea92ad5378
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2020-10-29 10:22:21 -0400

    i18n(export) renaming the URL by external link (#759)
    * i18n(export) renaming the URL by external link
    
    * refactor(icherche) lint
    
    * i18n(import-export) placeholder for layer

--- packages/geo/src/lib/import-export/shared/export.type.ts
+++ packages/geo/src/lib/import-export/shared/export.type.ts
@@ -1,6 +1,6 @@
 import { strEnum } from '@igo2/utils';
 
-export const ExportFormat = strEnum(['GeoJSON', 'GML', 'GPX', 'KML', 'Shapefile', 'CSVcomma', 'CSVsemicolon', 'URL']);
+export const ExportFormat = strEnum(['URL', 'GeoJSON', 'GML', 'GPX', 'KML', 'Shapefile', 'CSVcomma', 'CSVsemicolon']);
 export type ExportFormat = keyof typeof ExportFormat;
 
 export const EncodingFormat = strEnum(['UTF8', 'LATIN1']);
