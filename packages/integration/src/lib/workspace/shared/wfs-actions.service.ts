55ade9a3fc2255fc42c5f129cd0d0a8af3753229
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2020-11-11 08:37:19 -0500

    feat(workspace) allow to expand the workspace to 500px height (#774)
    * feat(workspace) allow to expand the workspace to 500px height
    
    * refactor(workspace) rename workspace full extent to maximize
    
    * wip
    
    * wip
    
    Co-authored-by: Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>

--- packages/integration/src/lib/workspace/shared/wfs-actions.service.ts
+++ packages/integration/src/lib/workspace/shared/wfs-actions.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { EventEmitter, Inject, Injectable, OnDestroy, Output } from '@angular/core';
 
 import { Action, Widget, EntityStoreFilterCustomFuncStrategy, EntityStoreFilterSelectionStrategy } from '@igo2/common';
 import { BehaviorSubject, Subscription } from 'rxjs';
@@ -12,15 +12,19 @@ import {
   OgcFilterWidget,
   OgcFilterableDataSource
 } from '@igo2/geo';
-import { StorageService, StorageScope, StorageServiceEvent } from '@igo2/core';
+import { StorageService, StorageScope, StorageServiceEvent, LanguageService, MediaService } from '@igo2/core';
 import { StorageState } from '../../storage/storage.state';
-import { skipWhile } from 'rxjs/operators';
+import { map, skipWhile } from 'rxjs/operators';
 import { ToolState } from '../../tool/tool.state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WfsActionsService implements OnDestroy  {
+
+  public maximize$: BehaviorSubject<boolean> = new BehaviorSubject(
+    this.storageService.get('workspaceMaximize') as boolean
+  );
 
   zoomAuto$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   private storageChange$$: Subscription;
@@ -37,10 +41,18 @@ export class WfsActionsService implements OnDestroy  {
     return this.storageService.get('rowsInMapExtent') as boolean;
   }
 
+  @Output() workspaceMaximizeEvent = new EventEmitter<boolean>();
+
   constructor(
     @Inject(OgcFilterWidget) private ogcFilterWidget: Widget,
     private storageState: StorageState,
+    public languageService: LanguageService,
+    private mediaService: MediaService,
     private toolState: ToolState) {}
+
+    getWorkspaceMaximizeEmitter(): EventEmitter<boolean> {
+      return this.workspaceMaximizeEvent;
+    }
 
   ngOnDestroy(): void {
     if (this.storageChange$$) {
@@ -147,6 +159,38 @@ export class WfsActionsService implements OnDestroy  {
         },
         args: [this.ogcFilterWidget, workspace]
       },
+      {
+        id: 'maximize',
+        title: this.languageService.translate.instant('igo.integration.workspace.maximize'),
+        tooltip: this.languageService.translate.instant(
+          'igo.integration.workspace.maximizeTooltip'
+        ),
+        icon: 'resize',
+        display: () => {
+          return this.maximize$.pipe(map((v) => !v && !this.mediaService.isMobile()));
+        },
+        handler: () => {
+          if (!this.mediaService.isMobile()) {
+            this.maximize$.next(true);
+          }
+        },
+      },
+      {
+        id: 'standardExtent',
+        title: this.languageService.translate.instant(
+          'igo.integration.workspace.standardExtent'
+        ),
+        tooltip: this.languageService.translate.instant(
+          'igo.integration.workspace.standardExtentTooltip'
+        ),
+        icon: 'resize',
+        display: () => {
+          return this.maximize$.pipe(map((v) => v && !this.mediaService.isMobile()));
+        },
+        handler: () => {
+          this.maximize$.next(false);
+        }
+      }
     ];
     return (workspace.layer.dataSource as OgcFilterableDataSource).ogcFilters$?.value?.enabled ?
     actions : actions.filter(action => action.id !== 'ogcFilter');
