9ec88d3654a0d7b4b93b3e20185ea9fe28ebf0d2
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2021-02-23 14:54:46 -0500

    Update workspace.utils.ts

--- packages/integration/src/lib/workspace/shared/workspace.utils.ts
+++ packages/integration/src/lib/workspace/shared/workspace.utils.ts
@@ -3,13 +3,13 @@ import {
     FeatureStoreSelectionStrategy,
     FeatureWorkspace,
     WfsWorkspace
-  } from '@igo2/geo';
+} from '@igo2/geo';
 
 
 export function  handleZoomAuto(workspace: FeatureWorkspace | WfsWorkspace, storageService) {
     const zoomStrategy = workspace.entityStore
-    .getStrategyOfType(FeatureStoreSelectionStrategy) as FeatureStoreSelectionStrategy;
+        .getStrategyOfType(FeatureStoreSelectionStrategy) as FeatureStoreSelectionStrategy;
     zoomStrategy.setMotion(storageService.get('zoomAuto') as boolean ? FeatureMotion.Default : FeatureMotion.None);
-  }
+}
 
 
