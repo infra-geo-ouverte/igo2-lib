9db7d4abf90df1627229f2fe6c55268bedf95b90
matrottier <trottier.marcandre@gmail.com>
2021-03-25 15:27:39 -0400

    feat(azure auth) upgrade msal lib

--- packages/auth/src/lib/auth-form/auth-microsoft.component.ts
+++ packages/auth/src/lib/auth-form/auth-microsoft.component.ts
@@ -3,15 +3,18 @@ import {
   ChangeDetectionStrategy,
   ApplicationRef,
   Output,
-  EventEmitter
+  EventEmitter,
+  Inject
 } from '@angular/core';
 
-import { BroadcastService, MsalService } from '@azure/msal-angular';
-import { AuthResponse as MsalAuthResponse } from 'msal';
 
+import { MsalBroadcastService, MsalService, MSAL_GUARD_CONFIG, MsalGuardConfiguration} from '@azure/msal-angular';
+import { InteractionStatus, AuthenticationResult, InteractionType, PopupRequest, SilentRequest,InteractionRequiredAuthError } from '@azure/msal-browser';
 import { ConfigService } from '@igo2/core';
 import { AuthMicrosoftOptions } from '../shared/auth.interface';
 import { AuthService } from '../shared/auth.service';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'igo-auth-microsoft',
@@ -21,40 +24,58 @@ import { AuthService } from '../shared/auth.service';
 })
 export class AuthMicrosoftComponent {
   private options: AuthMicrosoftOptions;
-
+  private readonly _destroying$ = new Subject<void>();
   @Output() login: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(
     private authService: AuthService,
     private config: ConfigService,
     private appRef: ApplicationRef,
-    private broadcastService: BroadcastService,
-    private msalService: MsalService
+    private broadcastService: MsalBroadcastService,
+    private msalService: MsalService,
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
   ) {
     this.options = this.config.getConfig('auth.microsoft') || {};
 
     if (this.options.clientId) {
-      this.broadcastService.subscribe('msal:loginSuccess', () => {
+      this.broadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
         this.checkAccount();
-      });
+      })
+     
     } else {
       console.warn('Microsoft authentification needs "clientId" option');
     }
   }
 
   public loginMicrosoft() {
-    this.msalService.loginPopup();
+    this.msalService.loginPopup({...this.msalGuardConfig.authRequest} as PopupRequest)
+    .subscribe((response: AuthenticationResult) => {
+      this.msalService.instance.setActiveAccount(response.account);
+    });
   }
 
   private checkAccount() {
-    this.msalService
-      .acquireTokenSilent({ scopes: ['user.read'] })
-      .then((response: MsalAuthResponse) => {
+    this.msalService.instance
+      .acquireTokenSilent(this.msalGuardConfig.authRequest as SilentRequest)
+      .then((response: AuthenticationResult) => {
         const token = response.accessToken;
         this.authService.loginWithToken(token, 'microsoft').subscribe(() => {
           this.appRef.tick();
           this.login.emit(true);
         });
-      });
+      })
+      .catch(async (error) => {
+        if (error instanceof InteractionRequiredAuthError) {
+            // fallback to interaction when silent call fails
+            return this.msalService.acquireTokenPopup(this.msalGuardConfig.authRequest as SilentRequest);
+        }
+        }).catch(error => {
+            console.log('Fuck');
+        });
   }
 }
