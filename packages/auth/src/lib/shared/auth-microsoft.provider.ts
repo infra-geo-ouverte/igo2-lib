fa27c58db5fdd7c394642d0e436dd338c4782de0
matrottier <trottier.marcandre@gmail.com>
2021-03-30 09:17:24 -0400

    feat(auth azure-b2c) version alpha 1

--- packages/auth/src/lib/shared/auth-microsoft.provider.ts
+++ packages/auth/src/lib/shared/auth-microsoft.provider.ts
@@ -32,6 +32,23 @@ export function MSALConfigFactory(config: ConfigService): PublicClientApplicatio
   return myMsalObj;
 }
 
+export function MSALConfigFactoryb2c(config: ConfigService): PublicClientApplication {
+
+  const msConf: BrowserAuthOptions = config.getConfig('auth.microsoftb2c.browserAuthOptions') || {};
+  
+  msConf.redirectUri = msConf.redirectUri || window.location.href;
+  msConf.authority = msConf.authority || 'https://login.microsoftonline.com/organizations';
+
+  const myMsalObj = new PublicClientApplication({
+    auth: msConf,
+    cache: {
+      cacheLocation: 'sessionStorage'
+    }
+  });
+
+  return myMsalObj;
+}
+
 
 export function MSALAngularConfigFactory(config: ConfigService): MsalGuardConfiguration {
 
@@ -45,18 +62,47 @@ export function MSALAngularConfigFactory(config: ConfigService): MsalGuardConfig
   };
 }
 
-export function provideAuthMicrosoft() {
-  return [
-    {
-      provide: MSAL_INSTANCE,
-      useFactory: MSALConfigFactory,
-      deps: [ConfigService]
-    },
-    {
-      provide: MSAL_GUARD_CONFIG,
-      useFactory: MSALAngularConfigFactory,
-      deps: [ConfigService]
-    },
-    MsalService
-  ];
+export function MSALAngularConfigFactoryb2c(config: ConfigService): MsalGuardConfiguration {
+
+  const msConf: BrowserAuthOptions = config.getConfig('auth.microsoftb2c.browserAuthOptions') || {};
+
+  return {
+    interactionType: InteractionType.Popup,
+    authRequest: {
+      scopes: [msConf.clientId]
+    }
+  };
+}
+
+export function provideAuthMicrosoft(type?: string) {
+  
+  if (type === 'b2c') {
+    return [
+      {
+        provide: MSAL_INSTANCE,
+        useFactory: MSALConfigFactoryb2c,
+        deps: [ConfigService]
+      },
+      {
+        provide: MSAL_GUARD_CONFIG,
+        useFactory: MSALAngularConfigFactoryb2c,
+        deps: [ConfigService]
+      },
+      MsalService
+    ];
+  } else {
+    return [
+      {
+        provide: MSAL_INSTANCE,
+        useFactory: MSALConfigFactory,
+        deps: [ConfigService]
+      },
+      {
+        provide: MSAL_GUARD_CONFIG,
+        useFactory: MSALAngularConfigFactory,
+        deps: [ConfigService]
+      },
+      MsalService
+    ];
+  }
 }
