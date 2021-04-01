b65d75e4198260e849444891c1a1d7829b2d8188
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2021-04-01 09:38:25 -0400

    lint

--- packages/integration/src/lib/search/search-results-tool/search-results-tool.component.ts
+++ packages/integration/src/lib/search/search-results-tool/search-results-tool.component.ts
@@ -223,7 +223,13 @@ export class SearchResultsToolComponent implements OnInit, OnDestroy {
 
       featureToHandleGeom.map(result => {
         if (searchResultsGeometryEnabled) {
-          result.entity.data.meta.style = getCommonVectorStyle(Object.assign({}, { feature: result.entity.data }, this.searchState.searchOverlayStyleFocus));
+          result.entity.data.meta.style =
+            getCommonVectorStyle(
+              Object.assign(
+                {},
+                { feature: result.entity.data },
+                this.searchState.searchOverlayStyleFocus
+              ));
           this.shownResultsGeometries.push(result.entity.data as Feature);
           this.map.queryResultsOverlay.addFeature(result.entity.data as Feature, FeatureMotion.None);
           if (this.hasFeatureEmphasisOnSelection) {
