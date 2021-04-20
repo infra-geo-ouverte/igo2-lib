e6179696ee714a893c5ab7e12b861dba0ecc6ffc
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2021-04-20 17:01:58 -0400

    feat(draw): draw points as icons (#837)
    * add params to clusterStyle
    
    * fix(*wfs-datasource) use formatOptions config in WFS format creation. issue #645 (#646)
    
    Co-authored-by: Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
    
    * to merge master
    
    * WIP
    
    * WIP
    
    * add(draw): icon maker
    
    * WIP
    
    * feat(about): add training guide url based on profil
    
    * lint
    
    * Fix(image): fix lint
    
    * fix(draw): fix lint
    
    * fix(draw): handle no icon and ng-color-picker css fixed
    
    * fix(draw): change icons config name
    
    Co-authored-by: Déreck Lynch <50959537+drekss@users.noreply.github.com>
    Co-authored-by: drekss <bovictor123>
    Co-authored-by: matrottier <trottier.marcandre@gmail.com>
    Co-authored-by: Marc-André Trottier <marc-andre.trottier@msp.gouv.qc.ca>
    Co-authored-by: Dereck Lynch <dereck.lynch47@hotmail.com>
    Co-authored-by: Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>

---
+++ packages/geo/src/lib/draw/shared/draw-icon.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { ConfigService } from '@igo2/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class DrawIconService {
+
+    protected icons: Array<string>;
+
+    constructor(
+      protected config: ConfigService
+    ) {
+        this.getIconsList();
+    }
+
+    getIcons() {
+        return this.icons;
+    }
+
+    getPath(): any {
+      console.log(this);
+      return this.config.getConfig('drawingTool.icons') || [];
+    }
+
+    getIconsList() {
+      this.icons = this.getPath();
+    }
+}
