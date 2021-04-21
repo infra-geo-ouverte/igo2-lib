039255352fc050e77480476ca8988ac43dde3c78
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2021-04-21 13:38:29 -0400

    feat(message): add an optional date range to message application

--- packages/core/src/lib/message/shared/message.interface.ts
+++ packages/core/src/lib/message/shared/message.interface.ts
@@ -1,5 +1,6 @@
 import { TemplateRef } from '@angular/core';
 import { MessageType } from './message.enum';
+import { Notification } from 'angular2-notifications';
 
 export interface Message {
   title?: string;
@@ -7,11 +8,12 @@ export interface Message {
   html?: string | TemplateRef<any>;
   icon?: string;
   type?: MessageType;
-  options?: any;
+  options?: MessageOptions;
   format?: 'text' | 'html';
 }
 
-export interface MessageOptions {
-  timeOut?: number;
+export interface MessageOptions extends Notification {
   template?: string;
+  from?: Date | string; // if the current date is the 2021-04-20, this date is included with this example new Date('2021-04-20')
+  to?: Date | string; // if the current date is the 2021-04-21, this date is excluded with this example new Date('2021-04-21')
 }
