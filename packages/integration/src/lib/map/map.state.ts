fd1f066e4c7f4e132530e2e33b06a4d6d3172f86
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-05-17 13:52:58 -0400

    refactor(map): scaleBar

--- packages/integration/src/lib/map/map.state.ts
+++ packages/integration/src/lib/map/map.state.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
 import { IgoMap, MapService, ProjectionService } from '@igo2/geo';
-import { ContextState } from '../context';
 
 /**
  * Service that holds the state of the map module
