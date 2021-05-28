8d8d5533b5959f773bebffe5698a66ee205f7bf9
Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>
2021-05-28 08:48:40 -0400

    lint

--- packages/geo/src/lib/datasource/shared/capabilities.service.ts
+++ packages/geo/src/lib/datasource/shared/capabilities.service.ts
@@ -252,7 +252,6 @@ export class CapabilitiesService {
     const timeFilter = this.getTimeFilter(layer);
     const timeFilterable = timeFilter && Object.keys(timeFilter).length > 0;
     const legendOptions = layer.Style ? this.getStyle(layer.Style) : undefined;
-      
     const extent = layer.EX_GeographicBoundingBox ?
         olproj.transformExtent(layer.EX_GeographicBoundingBox, 'EPSG:4326', 'EPSG:3857') :
         undefined;
