0228b0af2b6b4fe6bbb28a71c32668735e42334e
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-06-15 09:41:47 -0400

    fix(offline-menu): Region editor

--- packages/integration/src/lib/download/download-tool/download-tool.component.ts
+++ packages/integration/src/lib/download/download-tool/download-tool.component.ts
@@ -1,32 +1,8 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatProgressBar } from '@angular/material/progress-bar';
-import { MatSlider } from '@angular/material/slider';
+import { Component, OnInit } from '@angular/core';
 import { ToolComponent } from '@igo2/common';
-import { LayerListToolService } from '@igo2/geo';
-import { createFromTemplate } from 'ol/tileurlfunction.js';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { DownloadState } from '../download.state';
-import { TransferedTile } from '../TransferedTile';
-import { MessageService } from '@igo2/core';
-import { first, map, skip, takeUntil, takeWhile } from 'rxjs/operators';
-import { filter } from 'jszip';
-import { DownloadToolState } from './download-tool.state';
-import { MatInput } from '@angular/material/input';
-import { TileDownloaderService, DownloadRegionService } from '@igo2/core';
 // need to do the TODOs in tileDownloader beforehand
 // need to make prototype of the interface
 // need to create the all the methods
-
-export interface TileToDownload {
-  url: string;
-  coord: [number, number, number];
-  templateUrl: string;
-  tileGrid;
-}
-
-function getNumberOfTiles(deltaHeight: number) {
-  return (Math.pow(4, deltaHeight + 1) - 1) / 3;
-}
 
 @ToolComponent({
   name: 'download',
@@ -38,7 +14,6 @@ function getNumberOfTiles(deltaHeight: number) {
   templateUrl: './download-tool.component.html',
   styleUrls: ['./download-tool.component.scss']
 })
-
 export class DownloadToolComponent implements OnInit {
   constructor() {}
 
--- packages/integration/src/lib/download/download-tool/download-tool.state.ts
+++ packages/integration/src/lib/download/download-tool/download-tool.state.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { TileToDownload } from './download-tool.component';
-
-
+import { TileToDownload } from '../region-editor/region-editor.component';
 
 @Injectable({
    providedIn: 'root'
