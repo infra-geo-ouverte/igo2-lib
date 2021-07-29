8d89184391b662f4fa9a3043fb43f4cd952b354f
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-07-29 16:56:36 -0400

    feat(offline-menu): region-draw component

---
+++ packages/integration/src/lib/download/region-draw/region-draw.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { GeoJSONGeometry, GeometryFormFieldComponent, IgoMap } from '@igo2/geo';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'igo-region-draw',
+  templateUrl: './region-draw.component.html',
+  styleUrls: ['./region-draw.component.scss']
+})
+export class RegionDrawComponent implements OnInit {
+  @Input() map: IgoMap;
+  @Input() formControl: FormControl;
+  @Input() disabled: boolean = false;
+  
+  @ViewChild('geometryFormField') geometryFormField: GeometryFormFieldComponent;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  resetRegion() {
+    this.formControl = new FormControl();
+  }
+
+  printRegion() {
+    console.log(this.formControl);
+  }
+
+  get value$(): BehaviorSubject<GeoJSONGeometry> {
+    return this.geometryFormField.value$;
+  }
+}
