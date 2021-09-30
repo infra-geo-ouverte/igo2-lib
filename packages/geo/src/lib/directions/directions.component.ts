27083f3ed4122d72b3eb55f483d3937ff29310b2
Pierre-Etienne Lord <pe_lord@pm.me>
2021-09-29 22:56:41 -0400

    wip

--- packages/geo/src/lib/directions/directions.component.ts
+++ packages/geo/src/lib/directions/directions.component.ts
@@ -5,12 +5,20 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { LanguageService } from '@igo2/core';
 import { EntityStore, EntityStoreWatcher } from '@igo2/common';
 
+import * as olCondition from 'ol/events/condition';
+import * as olInteraction from 'ol/interaction';
+import * as olProj from 'ol/proj';
+import { TranslateEvent } from 'ol/interaction/Translate';
+import Collection from 'ol/Collection';
+import { SelectEvent } from 'ol/interaction/Select';
+
 import { DirectionOptions, FeatureWithDirection, FeatureWithStop, FeatureWithStopProperties, Stop } from './shared/directions.interface';
 import { combineLatest, Subscription } from 'rxjs';
 import { addDirectionToRoutesFeatureStore, addStopToStopsFeatureStore, addStopToStore, initRoutesFeatureStore, initStopsFeatureStore, updateStoreSorting } from './shared/directions.utils';
 import { FeatureStore } from '../feature/shared/store';
 import { Feature } from '../feature/shared/feature.interfaces';
 import { DirectionsService } from './shared/directions.service';
+import { DirectionType } from './shared/directions.enum';
 
 
 
@@ -28,6 +36,12 @@ export class DirectionsComponent implements OnInit, OnDestroy {
   private storeEmpty$$: Subscription;
   private storeChange$$: Subscription;
   private routesQueries$$: Subscription[] = [];
+
+  private selectStopInteraction: olInteraction.Select;
+  private translateStop: olInteraction.Translate;
+  private selectedRoute;
+  private focusOnStop: boolean = false;
+  private isTranslating: boolean = false;
 
   @Input() stopsStore: EntityStore<Stop>;
   @Input() stopsFeatureStore: FeatureStore<FeatureWithStop>;
@@ -47,7 +61,7 @@ export class DirectionsComponent implements OnInit, OnDestroy {
     setTimeout(() => {
       initStopsFeatureStore(this.stopsFeatureStore, this.languageService);
       initRoutesFeatureStore(this.routesFeatureStore, this.languageService);
-      // this.initOlInteraction();
+      this.initOlInteraction();
     }, 1);
 
   }
@@ -64,6 +78,76 @@ export class DirectionsComponent implements OnInit, OnDestroy {
     this.monitorEntityStoreChange();
   }
 
+  private initOlInteraction() {
+    this.selectStopInteraction = new olInteraction.Select({
+      layers: [this.stopsFeatureStore.layer.ol],
+      hitTolerance: 7,
+      condition: (event) => {
+        return event.type === 'pointermove' && !event.dragging;
+      }
+    });
+
+    this.translateStop = new olInteraction.Translate({
+      features: this.selectStopInteraction.getFeatures()
+    });
+    this.translateStop.on('translating', (evt: TranslateEvent) => {
+      this.isTranslating = true;
+      this.executeStopTranslation(evt.features);
+      // set overview mode
+    });
+    this.translateStop.on('translateend', (evt: TranslateEvent) => {
+      this.isTranslating = false;
+      this.executeStopTranslation(evt.features);
+    });
+
+    this.selectedRoute = new olInteraction.Select({
+      layers: [this.routesFeatureStore.layer.ol],
+      condition: olCondition.click,
+      hitTolerance: 7,
+      filter: feature => {
+        return feature.get('type') === DirectionType.Route &&
+          feature.get('active') &&
+          !this.isTranslating
+      }
+    });
+    this.selectedRoute.on('select', (evt: SelectEvent) => {
+      if (this.focusOnStop === false) {
+        const selectCoordinates = olProj.transform(
+          (evt as any).mapBrowserEvent.coordinate,
+          this.routesFeatureStore.layer.map.projection,
+          this.projection
+        );
+        const addedStop = addStopToStore(this.stopsStore);
+        addedStop.text = selectCoordinates.join(',');
+        addedStop.coordinates = [selectCoordinates[0], selectCoordinates[1]];
+      }
+    });
+
+    this.stopsFeatureStore.layer.map.ol.addInteraction(this.selectStopInteraction);
+    this.stopsFeatureStore.layer.map.ol.addInteraction(this.translateStop);
+    this.routesFeatureStore.layer.map.ol.addInteraction(this.selectedRoute);
+  }
+
+  private executeStopTranslation(
+    features: Collection<any>
+  ) {
+    if (features.getLength() === 0) {
+      return;
+    }
+    const firstFeature = features.getArray()[0];
+    const translatedStopId = firstFeature.getId();
+
+    const translationCoordinates = olProj.transform(
+      firstFeature.getGeometry().getCoordinates(),
+      this.stopsFeatureStore.layer.map.projection,
+      this.projection
+    );
+    const translatedStop = this.stopsStore.get(translatedStopId);
+    translatedStop.coordinates = [translationCoordinates[0], translationCoordinates[1]];
+    translatedStop.text = translationCoordinates.join(',');
+    this.stopsStore.update(translatedStop);
+    // todo refresh proposals
+  }
 
   private monitorEmptyEntityStore() {
     // Watch if the store is empty to reset it
@@ -86,7 +170,7 @@ export class DirectionsComponent implements OnInit, OnDestroy {
       .subscribe(() => {
         updateStoreSorting(this.stopsStore);
         this.handleStopsFeature();
-        this.getRoutes();
+        this.getRoutes(this.isTranslating);
       });
   }
 
@@ -130,6 +214,7 @@ export class DirectionsComponent implements OnInit, OnDestroy {
       steps: false,
       alternatives: false,
     };
+    this.routesQueries$$.map((u) => u.unsubscribe());
     const routeResponse = this.directionsService.route(
       roundedCoordinates,
       isOverview ? overviewDirectionsOptions : undefined
