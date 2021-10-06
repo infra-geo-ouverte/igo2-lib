a731c6b56bda50b0b61d68c3231ab0af8197601c
Pierre-Etienne Lord <pe_lord@pm.me>
2021-10-06 17:30:57 -0400

    wip

--- packages/utils/src/lib/number-utils.ts
+++ packages/utils/src/lib/number-utils.ts
@@ -1,6 +1,6 @@
 export class NumberUtils {
-    static roundToNDecimal(number: number, decimal: number = 3): number {
+    static roundToNDecimal(num: number, decimal: number = 3): number {
         const roundFactor = Math.pow(10, decimal);
-        return Math.round((number) * roundFactor) / roundFactor;
+        return Math.round((num) * roundFactor) / roundFactor;
     }
 }
