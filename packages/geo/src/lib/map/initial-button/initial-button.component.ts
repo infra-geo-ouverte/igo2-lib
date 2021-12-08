17f079f674b596ca06fb0c988177a064e14c0934
mneige <90412486+mneige@users.noreply.github.com>
2021-12-08 11:04:48 -0500

    adding initial button component files

---
+++ packages/geo/src/lib/map/initial-button/initial-button.component.ts
@@ -0,0 +1,47 @@
+import { ConfigService } from '@igo2/core';
+import { Component, Input } from '@angular/core';
+import { IgoMap } from '../shared/map';
+/*
+Button to center to initial the map extent
+*/
+@Component({
+  selector: 'igo-initial-button',
+  templateUrl:'./initial-button.component.html',
+  styleUrls: ['./initial-button.component.scss'],
+})
+export class InitialButtonComponent {
+  @Input()
+  get map(): IgoMap {
+    return this._map;
+  }
+  set map(value: IgoMap) {
+    this._map = value;
+  }
+  private _map: IgoMap;
+  @Input()
+  get color(): string {
+    return this._color;
+  }
+  set color(value: string) {
+    this._color = value;
+  }
+  private _color: string;
+  public initialExtent
+  public initialCenter
+  public initialZoom
+
+  constructor(public configService: ConfigService) {
+      this.initialExtent = this.configService.getConfig('hasInitialButton.initExtent'); // [-8000000, 5800000, -6800000, 6900000] MINX | MINY | MAXX | MAXY EPSG:3857 - WGS 84 / Pseudo-Mercator (meters)
+      this.initialCenter = this.configService.getConfig('hasInitialButton.initCenter') ; //  [-71.938087, 47.446975] 
+      this.initialZoom = this.configService.getConfig('hasInitialButton.initZoom') ; //  6
+  }
+
+onToggleClick($event){
+  if (this.initialExtent) {
+    this.map.viewController.zoomToExtent(this.initialExtent)
+  }
+   else if (this.initialCenter && this.initialZoom)  {
+    this.map.setView({center : this.initialCenter, zoom: this.initialZoom})
+  }
+}
+}
