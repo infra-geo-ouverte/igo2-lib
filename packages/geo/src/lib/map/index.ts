35c5de88fed85d2240b599e08979145797cbe236
mneige <90412486+mneige@users.noreply.github.com>
2021-12-14 16:42:02 -0500

    corrections to initial button

--- packages/geo/src/lib/map/index.ts
+++ packages/geo/src/lib/map/index.ts
@@ -4,6 +4,7 @@ export * from './map-browser';
 export * from './zoom-button';
 export * from './menu-button';
 export * from './geolocate-button';
+export * from './initial-button';
 export * from './offline-button';
 export * from './baselayers-switcher';
 export * from './rotation-button';
