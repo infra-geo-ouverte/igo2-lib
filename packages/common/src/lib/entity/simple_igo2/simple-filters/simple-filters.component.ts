b11394a4f4c81dea93b1d11beb4af66c4f31e122
MAXIME LAMER <maxime.lamer@msp.gouv.qc.ca>
2022-05-09 13:17:28 -0400

    worked of filters autocomplete

--- packages/common/src/lib/entity/simple_igo2/simple-filters/simple-filters.component.ts
+++ packages/common/src/lib/entity/simple_igo2/simple-filters/simple-filters.component.ts
@@ -1,26 +1,25 @@
 import { FeatureCollection } from 'geojson';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SimpleFilter, TypeValues, Value } from './simple-filters.interface';
 import { ConfigService } from '@igo2/core';
 import { map } from 'rxjs/operators';
-import { Observable, Subscription } from 'rxjs';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'igo-simple-filters',
   templateUrl: './simple-filters.component.html',
   styleUrls: ['./simple-filters.component.scss']
 })
-export class SimpleFiltersComponent implements OnInit, OnDestroy {
+export class SimpleFiltersComponent implements OnInit {
   public terrAPIBaseURL: string = "https://geoegl.msp.gouv.qc.ca/apis/terrapi/";
   public filtersConfig: Array<SimpleFilter>;
-  public typesValues: Array<TypeValues> = [];
-  public filteredTypesValues$: Observable<Array<TypeValues>>;
-  public typesValuesChange$$: Subscription;
-  public previousSpatialFiltersFormValue: any;
+  public allTypesValues: Array<TypeValues> = [];
+  public filteredTypesValues: Array<TypeValues> = [];
 
   public spatialFiltersForm: FormGroup;
+  public spatialFiltersFormPreviousValues: any;
 
   constructor(private configService: ConfigService, private http: HttpClient, private formBuilder: FormBuilder) { }
 
@@ -34,54 +33,21 @@ export class SimpleFiltersComponent implements OnInit, OnDestroy {
         const typeValues: TypeValues = await this.getValues(filter);
 
         if (typeValues) {
-          this.typesValues.push(typeValues);
+          this.allTypesValues.push(typeValues);
         }
       }
     }
+    this.filteredTypesValues = JSON.parse(JSON.stringify(this.allTypesValues));
 
-    this.typesValues.forEach((typeValues: TypeValues) => {
+    this.allTypesValues.forEach((typeValues: TypeValues) => {
       this.spatialFiltersForm.addControl(typeValues.type, this.formBuilder.control(''));
     });
 
-    this.spatialFiltersForm.valueChanges.subscribe(currentSpatialFiltersFormValue => {
-      console.log(currentSpatialFiltersFormValue)
-      const values: Array<Value> = [];
-      for (let type in currentSpatialFiltersFormValue) {
-        if (currentSpatialFiltersFormValue[type] instanceof Object) {
-          values.push(currentSpatialFiltersFormValue[type]);
-        }
-      }
+    this.spatialFiltersFormPreviousValues = this.spatialFiltersForm.value;
 
-      if (values.length > 0) {
-        for (let typeValues of this.typesValues) {
-          for (let value of values) {
-            if (typeValues.type !== value.type) {
-              const sourceType: string = value.type;
-              const sourceCode: string = value.code;
-              const targetType: string = typeValues.type;
-
-              const url: string = this.terrAPIBaseURL + `${sourceType}/${sourceCode}/${targetType}`;
-              const params: HttpParams = new HttpParams().set('sort', 'nom');
-
-              this.http.get<FeatureCollection>(url, {params}).pipe(map((featureCollection: FeatureCollection) => {
-                console.log(featureCollection)
-              }))
-            }
-          }
-        }
-      }
+    this.spatialFiltersForm.valueChanges.subscribe(spatialFiltersFormCurrentValues => {
+      this.filterValues(spatialFiltersFormCurrentValues);
     });
-  }
-
-
-  ngOnDestroy() {
-    this.typesValuesChange$$.unsubscribe();
-  }
-
-  filter(value) {
-    const filterValue = value.toLowerCase();
-    console.log(value)
-    return this.typesValues
   }
 
   displayName(value: Value) {
@@ -115,13 +81,62 @@ export class SimpleFiltersComponent implements OnInit, OnDestroy {
     return response;
   }
 
+  async createFilterList2(sourceType: string, sourceCode: string, targetType: string): Promise<FeatureCollection> {
+    const url: string = this.terrAPIBaseURL + `${sourceType}/${sourceCode}/${targetType}`;
+    const params: HttpParams = new HttpParams().set('sort', 'nom');
+    let response: FeatureCollection;
+
+    await this.http.get<FeatureCollection>(url, {params}).pipe(map((featureCollection: FeatureCollection) => {
+      response = featureCollection;
+    })).toPromise();
+
+    return response;
+  }
+
   getDescription(formControlName: string): string {
-    const typeValuesFiltered: TypeValues = this.typesValues.find((typeValues: TypeValues) => typeValues.type === formControlName);
+    const typeValuesFiltered: TypeValues = this.filteredTypesValues.find((typeValues: TypeValues) => typeValues.type === formControlName);
     return typeValuesFiltered.description;
   }
 
   getOptions(formControlName: string): Array<Value> {
-    return this.typesValues.find(typeValues => typeValues.type === formControlName).values;
+    return this.filteredTypesValues.find(typeValues => typeValues.type === formControlName).values;
+  }
+
+  async onSelection(event: MatAutocompleteSelectedEvent) {
+    const value: Value = event.option.value;
+    for (let typeValues of this.filteredTypesValues) {
+      if (value.type !== typeValues.type) {
+        const sourceType: string = value.type;
+        const sourceCode: string = value.code;
+        const targetType: string = typeValues.type;
+
+        const featureCollection = await this.createFilterList2(sourceType, sourceCode, targetType);
+
+        if (featureCollection) {
+          let values: Array<Value> = [];
+
+          featureCollection.features.forEach(feature => {
+            values.push({type: targetType, code: feature.properties.code, nom: feature.properties.nom});
+          });
+
+          typeValues.values = values;
+        }
+      }
+    }
+  }
+
+  filterValues(currentFormGroupValues: any) {
+    for (let control in currentFormGroupValues) {
+      const currentValue: string = currentFormGroupValues[control];
+      const previousValue: string = this.spatialFiltersFormPreviousValues[control];
+      if (currentValue !== previousValue && typeof currentValue === 'string') {
+        const allTypesValues: Array<TypeValues> = this.allTypesValues;
+        const values: Array<Value> = allTypesValues.find((typeValues: TypeValues) => typeValues.type === control).values;
+        const filteredValues: Array<Value> = values.filter((value: Value) => value.nom.toLowerCase().includes(currentValue.toLowerCase()));
+        this.filteredTypesValues.find((typeValue: TypeValues) => typeValue.type === control).values = filteredValues;
+      }
+    }
+    this.spatialFiltersFormPreviousValues = currentFormGroupValues;
   }
 
   onFilter() {
