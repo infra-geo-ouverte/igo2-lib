1a3f6f5335684aff37247b2db910296b579fdca8
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2022-05-24 13:17:25 -0400

    Label resolution (#1017)
    * feat(styleService): provide resolution to control if label is shown/hidden on styleByAttribute
    
    * feat(styleService): provide resolution to control if label is shown/hidden on createStyle
    
    * lint
    
    * lint
    
    Co-authored-by: Pierre-Étienne Lord <pe_lord@yahoo.ca>

--- packages/geo/src/lib/import-export/shared/import.utils.ts
+++ packages/geo/src/lib/import-export/shared/import.utils.ts
@@ -85,8 +85,8 @@ export function addLayerAndFeaturesStyledToMap(
       layerTitle.toString() + '.styleByAttribute'
     );
 
-    style = feature => {
-      return styleService.createStyleByAttribute(feature, styleByAttribute);
+    style = (feature, resolution) => {
+      return styleService.createStyleByAttribute(feature, styleByAttribute, resolution);
     };
   } else if (
     styleListService.getStyleList(layerTitle.toString() + '.clusterStyle')
@@ -98,16 +98,15 @@ export function addLayerAndFeaturesStyledToMap(
       layerTitle.toString() + '.distance'
     );
 
-    const baseStyle = styleService.createStyle(
-      styleListService.getStyleList(layerTitle.toString() + '.clusterStyle')
-    );
-
-    style = feature => {
-      return styleService.createClusterStyle(feature, clusterParam, baseStyle);
+    style = (feature, resolution) => {
+      const baseStyle = styleService.createStyle(
+        styleListService.getStyleList(layerTitle.toString() + '.clusterStyle'), feature, resolution
+      );
+      return styleService.createClusterStyle(feature, resolution, clusterParam, baseStyle);
     };
   } else if (styleListService.getStyleList(layerTitle.toString() + '.style')) {
-    style = styleService.createStyle(
-      styleListService.getStyleList(layerTitle.toString() + '.style')
+    style = (feature, resolution) => styleService.createStyle(
+      styleListService.getStyleList(layerTitle.toString() + '.style'), feature, resolution
     );
   } else if (
     styleListService.getStyleList('default.clusterStyle') &&
@@ -118,16 +117,15 @@ export function addLayerAndFeaturesStyledToMap(
     );
     distance = styleListService.getStyleList('default.distance');
 
-    const baseStyle = styleService.createStyle(
-      styleListService.getStyleList('default.clusterStyle')
-    );
-
-    style = feature => {
-      return styleService.createClusterStyle(feature, clusterParam, baseStyle);
+    style = (feature, resolution) => {
+      const baseStyle = styleService.createStyle(
+        styleListService.getStyleList('default.clusterStyle'), feature, resolution
+      );
+      return styleService.createClusterStyle(feature, resolution, clusterParam, baseStyle);
     };
   } else {
-    style = styleService.createStyle(
-      styleListService.getStyleList('default.style')
+    style = (feature, resolution) => styleService.createStyle(
+      styleListService.getStyleList('default.style'), feature, resolution
     );
   }
 
