40c43e6197de36f37f9f4c99f9d89366357932a7
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2022-07-19 10:25:27 -0400

    wip

--- packages/geo/src/lib/search/shared/sources/cadastre.ts
+++ packages/geo/src/lib/search/shared/sources/cadastre.ts
@@ -15,6 +15,8 @@ import { SearchSourceOptions, TextSearchOptions } from './source.interfaces';
 import { LanguageService, StorageService } from '@igo2/core';
 import { computeTermSimilarity } from '../search.utils';
 import { Cacheable } from 'ts-cacheable';
+import { GeoJsonGeometryTypes } from 'geojson';
+
 /**
  * Cadastre search source
  */
@@ -138,7 +140,7 @@ export class CadastreSearchSource extends SearchSource implements TextSearch {
       featureProjection: 'EPSG:4326'
     });
     return {
-      type: feature.getGeometry().getType(),
+      type: feature.getGeometry().getType() as GeoJsonGeometryTypes,
       coordinates: (feature.getGeometry() as any).getCoordinates()
     };
   }
