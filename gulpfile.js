4419f01ba438f9d631c484438703fc4ddc5efa67
Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>
2022-08-11 16:19:43 -0400

    feat(app): upgrade libs (outdated)

--- gulpfile.js
+++ gulpfile.js
@@ -159,6 +159,7 @@ gulp.task('prepublishOnly', done => {
 
 // https://github.com/stylus/stylus/pull/2538
 gulp.task('libs:fixUseAngularMaterialCDK', done => {
+  gulp.src(['./node_modules/@angular/material/core/focus-indicators/_private.scss']).pipe(replace(`@use '@angular/cdk';`, `@use './node_modules/@angular/cdk/_index.scss' as cdk;`)).pipe(gulp.dest('./node_modules/@angular/material/core/focus-indicators/'));
   gulp.src(['./node_modules/@angular/material/core/style/_menu-common.scss']).pipe(replace(`@use '@angular/cdk';`, `@use './node_modules/@angular/cdk/_index.scss' as cdk;`)).pipe(gulp.dest('./node_modules/@angular/material/core/style/'));
   gulp.src(['./node_modules/@angular/material/core/_core.scss']).pipe(replace(`@use '@angular/cdk';`, `@use './node_modules/@angular/cdk/_index.scss' as cdk;`)).pipe(gulp.dest('./node_modules/@angular/material/core/'));
   gulp.src(['./node_modules/@angular/material/core/ripple/_ripple.scss']).pipe(replace(`@use '@angular/cdk';`, `@use './node_modules/@angular/cdk/_index.scss' as cdk;`)).pipe(gulp.dest('./node_modules/@angular/material/core/ripple'));
