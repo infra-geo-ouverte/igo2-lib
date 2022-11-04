d20667840fa271c6233e6c69ea1a55084685a66f
Pierre-Etienne Lord <pe_lord@pm.me>
2022-11-04 08:08:54 -0400

    feat(geoProperties): method to detect if a feature contains geoService url and column (wms, arcgis...)

---
+++ packages/geo/src/lib/feature/shared/strategies/geo-properties.ts
@@ -0,0 +1,166 @@
+import { EntityStoreStrategy } from '@igo2/common';
+
+import { FeatureStore } from '../store';
+import { FeatureStorePropertyTypeStrategyOptions } from '../feature.interfaces';
+import { Subscription } from 'rxjs';
+import { PropertyTypeDetectorService } from '../../../utils/propertyTypeDetector/propertyTypeDetector.service';
+import { ObjectUtils } from '@igo2/utils';
+import { generateIdFromSourceOptions } from '../../../utils/id-generator';
+import { IgoMap } from '../../../map/shared/map';
+
+/**
+ * This strategy maintain the store features updated to detect geoproperties
+ * (wms/arcgis... layer and url).
+ * The features's state inside the map are tagged haveGeoServiceProperties = true;
+ */
+export class GeoPropertiesStrategy extends EntityStoreStrategy {
+
+  /**
+   * Subscription to the store's OL source changes
+   */
+  private stores$$ = new Map<FeatureStore, string>();
+  private states$$: Subscription[] = [];
+  private empty$$: Subscription;
+
+  /**
+   * The map the layer is bound to
+   */
+  private readonly map: IgoMap;
+
+  constructor(
+    protected options: FeatureStorePropertyTypeStrategyOptions,
+    private propertyTypeDetectorService: PropertyTypeDetectorService) {
+    super(options);
+    this.map = options.map;
+  }
+
+  /**
+   * Bind this strategy to a store and start watching for Ol source changes
+   * @param store Feature store
+   */
+  bindStore(store: FeatureStore) {
+    super.bindStore(store);
+    if (this.active === true) {
+      this.watchStore(store);
+    }
+  }
+
+  /**
+   * Unbind this strategy from a store and stop watching for Ol source changes
+   * @param store Feature store
+   */
+  unbindStore(store: FeatureStore) {
+    super.unbindStore(store);
+    if (this.active === true) {
+      this.unwatchStore(store);
+    }
+  }
+
+  /**
+   * Start watching all stores already bound to that strategy at once.
+   * @internal
+   */
+  protected doActivate() {
+    this.stores.forEach((store: FeatureStore) => this.watchStore(store));
+  }
+
+  /**
+   * Stop watching all stores bound to that strategy
+   * @internal
+   */
+  protected doDeactivate() {
+    this.unwatchAll();
+  }
+
+  /**
+   * Watch for a store's  OL source changes
+   * @param store Feature store
+   */
+  private watchStore(store: FeatureStore) {
+    if (this.stores$$.has(store)) {
+      return;
+    }
+
+    this.updateEntitiesPropertiesState(store);
+    this.states$$.push(store.entities$.subscribe((entities) => {
+      this.updateEntitiesPropertiesState(store);
+    }));
+  }
+
+  private updateEntitiesPropertiesState(store: FeatureStore) {
+    store.entities$.value.map(e => {
+      let isGeoService = false;
+      let entity;
+      // TODO NOTE Ensure to handle queryStore and FeatureStore(vector workspace)
+      if ((e as any).data) {
+        entity = (e as any).data;
+      } else {
+        entity = e;
+      }
+      for (const key of Object.keys(entity.properties)) {
+        const value = entity.properties[key];
+        isGeoService = this.propertyTypeDetectorService.isGeoService(value);
+        if (isGeoService) {
+          const geoService = this.propertyTypeDetectorService.getGeoService(value);
+          let layerName = entity.properties[geoService.columnForLayerName];
+          const appliedUrl = value;
+          let appliedLayerName = layerName;
+          let arcgisLayerName = undefined;
+
+          if (['arcgisrest', 'imagearcgisrest', 'tilearcgisrest'].includes(geoService.type)) {
+            arcgisLayerName = layerName;
+            appliedLayerName = undefined;
+          }
+
+          const so = ObjectUtils.removeUndefined({
+            sourceOptions: {
+              type: geoService.type || 'wms',
+              url: appliedUrl,
+              optionsFromCapabilities: true,
+              optionsFromApi: true,
+              params: {
+                LAYERS: appliedLayerName,
+                LAYER: appliedLayerName
+              }
+            }
+          });
+          const potentialLayerId = generateIdFromSourceOptions(so.sourceOptions);
+
+          const ns = {
+            geoService: {
+              added: this.map.layers.find(l => l.id === potentialLayerId) !== undefined,
+              haveGeoServiceProperties: true,
+              type: geoService.type,
+              url: appliedUrl,
+              layerName: appliedLayerName || arcgisLayerName
+            }
+          };
+
+          store.state.update(e, ns, true);
+          break;
+        }
+      }
+
+    });
+  }
+
+  /**
+   * Stop watching for a store's OL source changes
+   * @param store Feature store
+   */
+  private unwatchStore(store: FeatureStore) {
+    const key = this.stores$$.get(store);
+    if (key !== undefined) {
+      this.stores$$.delete(store);
+    }
+  }
+
+  /**
+   * Stop watching for OL source changes in all stores.
+   */
+  private unwatchAll() {
+    this.stores$$.clear();
+    this.states$$.map(state => state.unsubscribe());
+    if (this.empty$$) { this.empty$$.unsubscribe(); }
+  }
+}
---
+++ packages/geo/src/lib/utils/propertyTypeDetector/propertyTypeDetector.interface.ts
@@ -0,0 +1,19 @@
+export interface GeoServiceDefinition {
+  url: string;
+  type:
+  | 'wms'
+  | 'wfs'
+  | 'vector'
+  | 'wmts'
+  | 'xyz'
+  | 'osm'
+  | 'tiledebug'
+  | 'carto'
+  | 'arcgisrest'
+  | 'imagearcgisrest'
+  | 'tilearcgisrest'
+  | 'websocket'
+  | 'mvt'
+  | 'cluster';
+  columnForLayerName: string
+}
