f4bc48c38a3631254e99dac315f2cf0852d965af
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-02-17 14:09:39 -0500

    perf(vector-layer): delayed load of idb stored data

--- packages/geo/src/lib/layer/shared/layers/vector-layer.ts
+++ packages/geo/src/lib/layer/shared/layers/vector-layer.ts
@@ -25,7 +25,7 @@ import { WFSDataSourceOptions } from '../../../datasource/shared/datasources/wfs
 import { buildUrl, defaultMaxFeatures } from '../../../datasource/shared/datasources/wms-wfs.utils';
 import { OgcFilterableDataSourceOptions } from '../../../filter/shared/ogc-filter.interface';
 import { GeoNetworkService, SimpleGetOptions } from '../../../offline/shared/geo-network.service';
-import { catchError, concatMap, first } from 'rxjs/operators';
+import { catchError, concatMap, delay, first } from 'rxjs/operators';
 import { GeoDBService } from '../../../offline/geoDB/geoDB.service';
 import { of } from 'rxjs';
 export class VectorLayer extends Layer {
@@ -413,7 +413,7 @@ export class VectorLayer extends Layer {
       };
 
       const options: SimpleGetOptions = { responseType };
-      this.geoNetworkService.geoDBService.get(url).pipe(concatMap(r =>
+      this.geoNetworkService.geoDBService.get(url).pipe(delay(100)).pipe(concatMap(r =>
         r ? of(r) : this.geoNetworkService.get(modifiedUrl, options)
           .pipe(
             first(),
