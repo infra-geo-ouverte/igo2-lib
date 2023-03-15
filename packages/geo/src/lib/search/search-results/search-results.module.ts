1a79c2d6c2ddfc72b72af2fbd4a6f5b2e6dde228
Aziz <119948730+aziz-access@users.noreply.github.com>
2023-03-15 19:06:06 +0100

    feat(geo): allow query results to show results as tab instead of a list (#1156)
    * perf(geo): changer l'affichage de Query du liste collapse ver affichage par onglet
    
    * verify the result for toast-panel or search-result-tools
    
    * rename variable
    
    * add animation to resize tabs content
    
    ---------
    
    Co-authored-by: Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>

--- packages/geo/src/lib/search/search-results/search-results.module.ts
+++ packages/geo/src/lib/search/search-results/search-results.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -38,6 +39,7 @@ import { SaveFeatureDialogComponent } from './save-feature-dialog.component';
     MatIconModule,
     MatListModule,
     MatButtonModule,
+    MatTabsModule,
     MatDialogModule,
     IgoCollapsibleModule,
     IgoListModule,
