7a817542b476a124042acfab8c9c2ea332f6a16c
Aziz <119948730+aziz-access@users.noreply.github.com>
2023-03-15 15:01:42 +0100

    fix(geo): solve absence of legend when printing an image (#1191)
    * fix(geo): solve absence of legend when printing an image
    
    * add legend if option is new page
    
    * add legend with position to map and solve the new page bug
    
    * add legend according to the position criterion and solve new page bug
    
    * add image background white if title, coment... added and download two images if new page selected
    
    * change translation when adding the legend on a new image

--- packages/geo/src/lib/print/shared/print.service.ts
+++ packages/geo/src/lib/print/shared/print.service.ts
@@ -736,17 +736,17 @@ export class PrintService {
     format = 'png',
     projection = false,
     scale = false,
-    legend = false,
     title = '',
     subtitle = '',
     comment = '',
-    doZipFile = true
+    doZipFile = true,
+    legendPosition: string
   ) {
     const status$ = new Subject();
     // const resolution = map.ol.getView().getResolution();
     this.activityId = this.activityService.register();
     const translate = this.languageService.translate;
-    map.ol.once('rendercomplete', (event: any) => {
+    map.ol.once('rendercomplete', async (event: any) => {
       format = format.toLowerCase();
       const oldCanvas = event.target
         .getViewport()
@@ -755,8 +755,6 @@ export class PrintService {
       const newContext = newCanvas.getContext('2d');
       // Postion in height to set the canvas in new canvas
       let positionHCanvas = 0;
-      // Position in width to set the Proj/Scale in new canvas
-      let positionWProjScale = 10;
       // Get height/width of map canvas
       const width = oldCanvas.width;
       let height = oldCanvas.height;
@@ -778,12 +776,20 @@ export class PrintService {
       // Set the new canvas with the new calculated size
       newCanvas.width = width;
       newCanvas.height = height;
-      // Patch Jpeg default black background to white
-      if (format === 'jpeg') {
-        newContext.fillStyle = '#ffffff';
-        newContext.fillRect(0, 0, width, height);
-        newContext.fillStyle = '#000000';
+      if (['bmp','gif', 'jpeg', 'png', 'tiff'].indexOf(format) > -1) {
+        // Patch Jpeg default black background to white
+        if (format === 'jpeg') {
+          newContext.fillStyle = '#ffffff';
+          newContext.fillRect(0, 0, width, height);
+          newContext.fillStyle = '#000000';
+        } else if (title !== '' || subtitle !== '' || comment !== '' ||
+            projection !== false || scale !== false) {
+              newContext.fillStyle = '#ffffff';
+              newContext.fillRect(0, 0, width, height);
+              newContext.fillStyle = '#000000';
+        }
       }
+
       // If a title need to be added to canvas
       if (title !== '') {
         // Set font for title
@@ -803,29 +809,23 @@ export class PrintService {
       }
       // Set font for next section
       newContext.font = '20px Calibri';
-      // If projection need to be added to canvas
-      if (projection !== false) {
-        const projText = translate.instant('igo.geo.printForm.projection');
-        newContext.textAlign = 'start';
-        newContext.fillText(
-          projText + ': ' + map.projection,
-          positionWProjScale,
-          positionHProjScale
-        );
-        positionWProjScale += 200; // Width position change for scale position
+      // If projection or/end scale need to be added to canvas
+      if((projection !== false) || (scale !== false)) {
+        let projectionScaleText = '';
+        if (projection !== false) {
+          const projText = translate.instant('igo.geo.printForm.projection');
+          projectionScaleText = projText + ': ' + map.projection + '         ';
+        }
+
+        if (scale !== false) {
+          const scaleText = translate.instant('igo.geo.printForm.scale');
+          const mapScale = map.viewController.getScale(resolution);
+          projectionScaleText += scaleText + ': ~ 1 / ' + formatScale(mapScale);
+        }
+        newContext.textAlign = 'center';
+        newContext.fillText(projectionScaleText, width / 2, positionHProjScale, width * 0.9);
       }
 
-      // If scale need to be added to canvas
-      if (scale !== false) {
-        const scaleText = translate.instant('igo.geo.printForm.scale');
-        const mapScale = map.viewController.getScale(resolution);
-        newContext.textAlign = 'start';
-        newContext.fillText(
-          scaleText + ': ~ 1 / ' + formatScale(mapScale),
-          positionWProjScale,
-          positionHProjScale
-        );
-      }
       // If a comment need to be added to canvas
       if (comment !== '') {
         newContext.textAlign = 'center';
@@ -869,8 +869,22 @@ export class PrintService {
           }
         }
       }
+
       // Add map to new canvas
       newContext.drawImage(oldCanvas, 0, positionHCanvas);
+
+      // Check the legendPosition
+      if (legendPosition !== 'none') {
+        if (['topleft', 'topright', 'bottomleft', 'bottomright'].indexOf(legendPosition) > -1) {
+          await this.addLegendToImage(
+            newCanvas,
+            map,
+            resolution,
+            legendPosition,
+            format
+          );
+        }
+      }
 
       let status = SubjectStatus.Done;
       let fileNameWithExt = 'map.' + format;
@@ -914,6 +928,78 @@ export class PrintService {
     map.ol.renderSync();
 
     return status$;
+  }
+
+  /**
+   * Create and Add Legend to the map canvas
+   * @param  canvas Canvas of the map
+   * @param  map Map of the app
+   * @param  resolution Resolution of map
+   * @param  legendPosition Legend position
+   * @param  format Image format
+   */
+  private async addLegendToImage(
+      canvas: HTMLCanvasElement,
+      map: IgoMap,
+      resolution: number,
+      legendPosition: string,
+      format: string
+  ) {
+    const fileNameWithExt = 'map.' + format;
+    const context = canvas.getContext('2d');
+
+    // Get html code for the legend
+    const html = await this.getLayersLegendHtml(
+      map,
+      canvas.width,
+      resolution
+    ).toPromise();
+    // If no legend, save the map directly
+    if (html === '') {
+      await this.saveCanvasImageAsFile(canvas, fileNameWithExt, format);
+      return true;
+    }
+    // Create div to contain html code for legend
+    const div = window.document.createElement('div');
+    div.style.position = 'absolute';
+    div.style.top = '0';
+    // Add html code to convert in the new window
+    window.document.body.appendChild(div);
+    div.innerHTML = html;
+    await this.timeout(1);
+    const canvasLegend = await html2canvas(div, { useCORS: true }).catch((e) => {
+      console.log(e);
+    });
+
+    if (canvasLegend) {
+      const canvasHeight = canvas.height;
+      const canvasWidth = canvas.width;
+      const legendHeight = canvasLegend.height;
+      const legendWidth = canvasLegend.width;
+      // Move the legend to the correct position on the page
+      const offset = canvasHeight * 0.01;
+      let legendX: number;
+      let legendY: number;
+
+      if (legendPosition === 'bottomright') {
+        legendX = canvasWidth - legendWidth - offset;
+        legendY = canvasHeight - legendHeight - offset;
+      } else if (legendPosition === 'topright') {
+        legendX = canvasWidth - legendWidth - offset;
+        legendY = offset;
+      } else if (legendPosition === 'bottomleft') {
+        legendX = offset;
+        legendY = canvasHeight - legendHeight - offset;
+      } else if (legendPosition === 'topleft') {
+        legendX = offset;
+        legendY = offset;
+      }
+
+      context.drawImage(canvasLegend, legendX, legendY, legendWidth, legendHeight);
+      context.strokeRect(legendX, legendY, legendWidth, legendHeight);
+      div.parentNode.removeChild(div); // remove temp div (IE style)
+      return true;
+    }
   }
 
   private renderMap(map, size, extent) {
