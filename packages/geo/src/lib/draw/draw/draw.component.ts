327e5d3f9b7061c563d140a8c89170543543707e
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2023-03-15 15:58:48 -0400

    fix(draw): error with layers with numeric id (#1195)

--- packages/geo/src/lib/draw/draw/draw.component.ts
+++ packages/geo/src/lib/draw/draw/draw.component.ts
@@ -1164,7 +1164,7 @@ export class DrawComponent implements OnInit, OnDestroy {
 
   get allLayers() {
     return this.map.layers.filter((layer) =>
-      layer.id.includes('igo-draw-layer')
+      String(layer.id).includes('igo-draw-layer')
     );
   }
 
