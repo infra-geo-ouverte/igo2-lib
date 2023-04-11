e7a17ecc8e983ff94fc9b70e75c66183d8b11c46
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-04-11 12:41:50 -0400

    lint

--- packages/integration/src/lib/analytics/analytics-listener.service.ts
+++ packages/integration/src/lib/analytics/analytics-listener.service.ts
@@ -116,19 +116,22 @@ export class AnalyticsListenerService {
             const wmtsLayerName: string = wmtsDataSource.layer;
             const wmtsUrl: string = wmtsDataSource.url;
             const wmtsMatrixSet: string = wmtsDataSource.matrixSet;
-            wmtsParams = 'nom de la couche:'+' '+ wmtsLayerName +','+' '+'url:'+' '+ wmtsUrl +','+' '+'référence spatiale:'+' '+ wmtsDataSource.matrixSet ;
+            wmtsParams = 'nom de la couche:' + ' ' + wmtsLayerName + ',' + ' ' + 'url:' + ' '
+            wmtsUrl + ',' + ' ' + 'référence spatiale:' + ' ' + wmtsDataSource.matrixSet;
             break;
           case 'arcgisrest':
           case 'tilearcgisrest':
           case 'imagearcgisrest':
-            const restDataSource = layer.dataSource.options as ArcGISRestDataSourceOptions | TileArcGISRestDataSourceOptions |ArcGISRestImageDataSourceOptions ;
+            const restDataSource = layer.dataSource.options as ArcGISRestDataSourceOptions |
+              TileArcGISRestDataSourceOptions | ArcGISRestImageDataSourceOptions;
             const arcgisrestName: string = restDataSource._layerOptionsFromSource.title;
             const arcgisrestUrl: string = restDataSource.url;
             const tilearcgisrestName: string = restDataSource._layerOptionsFromSource.title;
             const tilearcgisrestUrl: string = restDataSource.url;
             const imagearcgisrestName: string = restDataSource._layerOptionsFromSource.title;
             const imagearcgisrestUrl: string = restDataSource.url;
-            argisrestParams ='nom de la couche:'+' '+ arcgisrestName || tilearcgisrestName || imagearcgisrestName +','+' '+'url:'+' '+ arcgisrestUrl || tilearcgisrestUrl || imagearcgisrestUrl;
+            argisrestParams = 'nom de la couche:' + ' ' + arcgisrestName || tilearcgisrestName || imagearcgisrestName +
+              ',' + ' ' + 'url:' + ' ' + arcgisrestUrl || tilearcgisrestUrl || imagearcgisrestUrl;
             break;
           case 'xyz':
             const xyzDataSource = layer.dataSource.options as XYZDataSourceOptions;
@@ -139,7 +142,8 @@ export class AnalyticsListenerService {
 
 
         }
-            this.analyticsService.trackLayer('layer', 'addLayer', layer.dataSource.options.type, wmsParams || wmtsParams || argisrestParams || tilearcgisrestParams || xyzParams || imagearcgisrestParams);
+        this.analyticsService.trackLayer('layer', 'addLayer', layer.dataSource.options.type,
+          wmsParams || wmtsParams || argisrestParams || tilearcgisrestParams || xyzParams || imagearcgisrestParams);
       });
     });
   }
