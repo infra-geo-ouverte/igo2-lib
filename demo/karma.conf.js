04dab7a7266367a9b25973385b85657ca64b43e5
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-06-20 14:54:54 -0400

    feat(E2E): setup E2E testing with Cypress for the demo project

--- demo/karma.conf.js
+++ demo/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
+      dir: require('path').join(__dirname, '../coverage'),
       reporters: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
