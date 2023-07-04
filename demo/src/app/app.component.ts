958d586efbc92820036073be8b69967ef36d043c
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-07-04 15:46:18 -0400

    fix - fix eslint ignore Packages - add IGO favicon - add splash-screen - fix sidenav background - fix and standardize font-family - fix button-toggle override

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import {
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-} from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnDestroy } from '@angular/core';
 
-import { userAgent } from '@igo2/utils';
+import { userAgent, DomUtils } from '@igo2/utils';
 import { version } from '@igo2/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { first, timeout } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -21,14 +20,45 @@ export class AppComponent implements OnDestroy {
   private _mobileQueryListener: () => void;
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private changeDetectorRef: ChangeDetectorRef,
     private media: MediaMatcher,
+    private router: Router,
   ) {
     this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
 
     this.detectOldBrowser();
+
+    this.handleSplashScreen();
+  }
+
+  private handleSplashScreen(): void {
+    this.router.events
+      .pipe(
+        first((events) => events instanceof NavigationEnd),
+        timeout(1000)
+      )
+      .subscribe(() => {
+        this._removeSplashScreen();
+      });
+  }
+
+  private _removeSplashScreen(): void {
+    const intro = this.document.getElementById('splash-screen');
+    if (!intro) {
+      return;
+    }
+    intro.classList.add('is-destroying');
+
+    const destroyingAnimationTime = 300;
+    const stylesheet = this.document.getElementById('splash-screen-stylesheet');
+
+    setTimeout(() => {
+      DomUtils.remove(intro);
+      DomUtils.remove(stylesheet);
+    }, destroyingAnimationTime);
   }
 
   ngOnDestroy(): void {
