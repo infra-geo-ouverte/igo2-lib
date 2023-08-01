30f311fa722e8db9b0f3a0e95a7e3ab1aeb08eaf
alecarn <133774929+alecarn@users.noreply.github.com>
2023-08-01 15:02:49 -0400

    Update angular v16 (#1329)
    * Update angular v16 and other dependencies
    
    * fix(ExampleViewer): define imports
    
    * fix lint
    
    * fix(Types): conflict between Cypress and Jasmine
    
    * fix packages dependencies
    
    * fix(styles): encapsulate style in host to avoid conflict
    
    * WIP
    
    * fix styles

--- demo/src/app/components/example/example-viewer/example-viewer.component.spec.ts
+++ demo/src/app/components/example/example-viewer/example-viewer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExampleViewerComponent } from './example-viewer.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 describe('ExampleViewerComponent', () => {
   let component: ExampleViewerComponent;
@@ -8,6 +10,10 @@ describe('ExampleViewerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MatCardModule,
+        MatDividerModule,
+      ],
       declarations: [ ExampleViewerComponent ]
     })
     .compileComponents();
