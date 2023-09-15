221d5d44827bddd1ec865e1e6882fc3dcc0484c8
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-09-15 15:18:00 -0400

    fix(geo): google map links were not interpreted on mobile

--- packages/geo/src/lib/utils/googleLinks.ts
+++ packages/geo/src/lib/utils/googleLinks.ts
@@ -1,14 +1,14 @@
 export class GoogleLinks {
   static getGoogleMapsCoordLink(lon, lat) {
-    return 'https://www.google.com/maps?q=' + lat + ',' + lon;
+    return `https://www.google.com/maps/search/?api=1&query=${lat}%2C${lon}`;
   }
 
   static getGoogleStreetViewLink(lon, lat) {
-    return 'https://www.google.com/maps?q=&layer=c&cbll=' + lat + ',' + lon;
+    return `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${lat}%2C${lon}`
   }
 
   static getGoogleMapsNameLink(name) {
     const encodedName = encodeURI(name);
-    return 'https://www.google.com/maps?q=' + encodedName;
+    return `https://www.google.com/maps/search/?api=1&query=${encodedName}`;
   }
 }
