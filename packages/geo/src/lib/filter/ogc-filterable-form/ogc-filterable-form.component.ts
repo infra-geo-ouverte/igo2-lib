8dc2de3ac6b1479b5a7c51534e6d813328367062
aziz <aziz@access-international.dev>
2023-09-20 20:58:45 +0200

    add providers mat_autocomplite and mat_select config to set overly fit-content

--- packages/geo/src/lib/filter/ogc-filterable-form/ogc-filterable-form.component.ts
+++ packages/geo/src/lib/filter/ogc-filterable-form/ogc-filterable-form.component.ts
@@ -1,10 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { OgcFilterableDataSource } from '../shared/ogc-filter.interface';
 import { IgoMap } from '../../map/shared';
+import { MAT_SELECT_CONFIG } from '@angular/material/select';
+import { MAT_AUTOCOMPLETE_DEFAULT_OPTIONS } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'igo-ogc-filterable-form',
-  templateUrl: './ogc-filterable-form.component.html'
+  templateUrl: './ogc-filterable-form.component.html',
+  providers: [
+    {
+      provide: MAT_SELECT_CONFIG,
+      useValue: { overlayPanelClass: 'igo-overlay-panel-width' }
+    },
+    {
+      provide: MAT_AUTOCOMPLETE_DEFAULT_OPTIONS,
+      useValue: { overlayPanelClass: 'igo-overlay-panel-width' }
+    }
+  ]
 })
 export class OgcFilterableFormComponent {
   @Input() datasource: OgcFilterableDataSource;
