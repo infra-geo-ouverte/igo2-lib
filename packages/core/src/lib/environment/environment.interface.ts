4539f800320d27cc18ff5aa887fe8bbd5f85e772
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-10-03 13:32:57 -0400

    chore(core): set type for each configService.getConfig usage

--- packages/core/src/lib/environment/environment.interface.ts
+++ packages/core/src/lib/environment/environment.interface.ts
@@ -1,10 +1,17 @@
+import { AnalyticsOptions } from '../analytics';
 import { LanguageOptions } from '../language';
+import { MessageOptions } from '../message';
+import { RegexOptions } from '../regex';
 
 export interface BaseEnvironmentOptions {
   production: boolean;
 }
 
 export interface CoreOptions {
+  analytics?: AnalyticsOptions;
+  emailAddress?: string;
   language?: LanguageOptions;
+  message?: MessageOptions;
+  regex?: RegexOptions;
   version?: string;
 }
