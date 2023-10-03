375e26e793b93a26a65e2bf49bdde18cec26e548
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-10-03 15:13:45 -0400

    feat(EntityTableAutocompleteField): add basic test

--- packages/common/src/lib/entity/entity-table-fields/entity-table-autocomplete-field/entity-table-autocomplete-field.component.spec.ts
+++ packages/common/src/lib/entity/entity-table-fields/entity-table-autocomplete-field/entity-table-autocomplete-field.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EntityTableAutocompleteFieldComponent } from './entity-table-autocomplete-field.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { Component } from '@angular/core';
+import { EntityRecord, EntityTableColumn } from '../../shared';
+import { FormControl, FormGroup } from '@angular/forms';
 
 describe('EntityTableAutocompleteFieldComponent', () => {
   let component: EntityTableAutocompleteFieldComponent;
-  let fixture: ComponentFixture<EntityTableAutocompleteFieldComponent>;
+  let fixture: ComponentFixture<TestWrapperComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EntityTableAutocompleteFieldComponent]
+      declarations: [EntityTableAutocompleteFieldComponent],
+      imports: [MatAutocompleteModule, MatInputModule]
     });
-    fixture = TestBed.createComponent(EntityTableAutocompleteFieldComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestWrapperComponent);
+    component = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
@@ -19,3 +25,31 @@ describe('EntityTableAutocompleteFieldComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'igo-test-wrapper-component',
+  template: `<igo-entity-table-autocomplete-field
+    [control]="this.formGroup.get([column.name])"
+    [column]="column"
+    [record]="record"
+  ></igo-entity-table-autocomplete-field>`
+})
+class TestWrapperComponent {
+  formGroup: FormGroup;
+  column: EntityTableColumn = {
+    name: 'test',
+    title: 'Test'
+  };
+  record: EntityRecord<any> = {
+    entity: {},
+    state: {},
+    revision: 0,
+    ref: 'test'
+  };
+
+  constructor() {
+    this.formGroup = new FormGroup({
+      [this.column.name]: new FormControl()
+    });
+  }
+}
