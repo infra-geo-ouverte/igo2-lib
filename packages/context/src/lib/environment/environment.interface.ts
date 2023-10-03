a0ef05aaecad8845965ff4a4e67771ffec0dc767
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-10-03 11:59:00 -0400

    chore(context): set type for each configService.getConfig usage

--- packages/context/src/lib/environment/environment.interface.ts
+++ packages/context/src/lib/environment/environment.interface.ts
@@ -1,6 +1,10 @@
+import { ImportExportServiceOptions } from '@igo2/geo';
 import { ContextServiceOptions } from '../context-manager';
+import { AuthOptions } from '@igo2/auth';
 
 export interface ContextOptions {
+  auth?: AuthOptions;
   context?: ContextServiceOptions;
   favoriteContext4NonAuthenticated?: boolean;
+  importExport?: ImportExportServiceOptions;
 }
