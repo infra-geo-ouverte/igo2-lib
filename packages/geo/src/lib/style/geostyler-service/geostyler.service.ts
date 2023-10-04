133689b2e74975fe620d80ddc3495e392cc14d48
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-10-04 15:53:22 -0400

    feat(geostyler): add a method to generate legend from geostyler style

--- packages/geo/src/lib/style/geostyler-service/geostyler.service.ts
+++ packages/geo/src/lib/style/geostyler-service/geostyler.service.ts
@@ -1,47 +1,48 @@
 import { Injectable } from '@angular/core';
 import { OlStyleParser as OpenLayersParser } from 'geostyler-openlayers-parser';
-import { Style as GeoStylerStyle, WriteStyleResult } from 'geostyler-style';
-import { Observable, from } from 'rxjs';
-import { StyleSourceType } from '../shared';
+import LegendRenderer from 'geostyler-legend/dist/LegendRenderer/LegendRenderer';
 
+import { Style as GeoStylerStyle, WriteStyleResult } from 'geostyler-style';
+import { Observable, from, map } from 'rxjs';
+import { StyleSourceType } from '../shared';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class GeostylerStyleService {
   /**
-     * Create a style based on a object as
-     * "igoStyle": {
-     *       "geoStyler": {
-     *         "name": "GeoStyler Test",
-     *         "symbolizers":[
-     *           {
-     *           "kind": "Fill",
-     *           "color": "#ff0000",
-     *           "width": 5
-     *           }
-     *         ],
-     *         "scaleDenominator": {
-     *           "min": 50,
-     *           "max": 200
-     *         }
-     *       }
-     *     }
-     *
-     * @param options
-     * @returns
-     */
+   * Create a style based on a object as
+   * "igoStyle": {
+   *       "geoStyler": {
+   *         "name": "GeoStyler Test",
+   *         "symbolizers":[
+   *           {
+   *           "kind": "Fill",
+   *           "color": "#ff0000",
+   *           "width": 5
+   *           }
+   *         ],
+   *         "scaleDenominator": {
+   *           "min": 50,
+   *           "max": 200
+   *         }
+   *       }
+   *     }
+   *
+   * @param options
+   * @returns
+   */
   geostylerToOl(options: GeoStylerStyle): Observable<WriteStyleResult> {
     return this.geostylerTo(options, StyleSourceType.OpenLayers);
-
   }
-  private geostylerTo(options: GeoStylerStyle, destStyle: StyleSourceType): Observable<WriteStyleResult> {
+  private geostylerTo(
+    options: GeoStylerStyle,
+    destStyle: StyleSourceType
+  ): Observable<WriteStyleResult> {
     let parser: OpenLayersParser; // Ajouter les autres parser géré OU créer un type
     if (destStyle === StyleSourceType.OpenLayers) {
       parser = new OpenLayersParser();
-    } else
-    if(destStyle === StyleSourceType.HoverStyle){
+    } else if (destStyle === StyleSourceType.HoverStyle) {
       parser = new OpenLayersParser();
     }
     if (parser) {
@@ -49,7 +50,36 @@ export class GeostylerStyleService {
     }
   }
 
-  private hoverStyleToGeostyler(){
-    
+  public getStylerStyleToLegend(
+    type: string, // todo enum a faire
+    styles: GeoStylerStyle[],
+    width: number = 300,
+    height: number = 300
+  ): Observable<string> {
+    // todo define height automatically?
+    const renderer = new LegendRenderer({
+      maxColumnWidth: 300,
+      maxColumnHeight: 300,
+      overflow: 'auto',
+      styles,
+      size: [width, height],
+      hideRect: true
+    });
+    return from(renderer.renderAsImage('svg')).pipe(
+      map((r: Element) => {
+        const serializer = new XMLSerializer();
+        const svgXmlString = serializer.serializeToString(r);
+        if (type === 'svg') {
+          return svgXmlString;
+        } else {
+          const blob = new Blob([svgXmlString], {
+            type: 'image/svg+xml'
+          });
+          const urlCreator = window.URL;
+          return urlCreator.createObjectURL(blob);
+        }
+      })
+    );
   }
+  private hoverStyleToGeostyler() {}
 }
