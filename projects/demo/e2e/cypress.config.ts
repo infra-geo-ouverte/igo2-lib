d1af26ae6f89d5c469f0d71ecde5ac03c563f4f7
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-10-27 15:41:49 -0400

    path adjustment

--- projects/demo/e2e/cypress.config.ts
+++ projects/demo/e2e/cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    specPattern: 'demo/e2e/src/**/*.spec.ts',
-    supportFile: 'demo/e2e/support/e2e.ts',
-    videosFolder: "demo/e2e/dist/videos",
-    screenshotsFolder: "demo/e2e/dist/screenshots",
+    specPattern: 'projects/demo/e2e/src/**/*.spec.ts',
+    supportFile: 'projects/demo/e2e/support/e2e.ts',
+    videosFolder: 'projects/demo/e2e/dist/videos',
+    screenshotsFolder: 'projects/demo/e2e/dist/screenshots'
   }
 });
