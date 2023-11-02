a38649846bff35de655d43313c061e448a3e9e99
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-11-02 12:06:09 -0400

    fix(core): monitoring test config

--- packages/core/src/lib/monitoring/monitoring.provider.spec.ts
+++ packages/core/src/lib/monitoring/monitoring.provider.spec.ts
@@ -1,7 +1,7 @@
-import { provideMonitoring, MONITORING_OPTIONS } from './monitoring.provider';
 import { BaseEnvironmentOptions } from '../environment';
-import { MonitoringOptions } from './shared';
 import { MOCK_MONITORING_OPTIONS } from './__mocks__/monitoring-mock';
+import { MONITORING_OPTIONS, provideMonitoring } from './monitoring.provider';
+import { MonitoringOptions } from './shared';
 
 describe('Provide Monitoring', () => {
   let environment: BaseEnvironmentOptions;
@@ -9,22 +9,19 @@ describe('Provide Monitoring', () => {
 
   beforeEach(() => {
     environment = {
-      production: true,
-      igo: {
-        monitoring: { ...MOCK_MONITORING_OPTIONS }
-      }
+      production: true
     };
-    options = environment.igo.monitoring;
+    options = { ...MOCK_MONITORING_OPTIONS };
   });
 
   it('should return null if monitoring options are not defined', () => {
-    environment.igo.monitoring = null;
-    const providers = provideMonitoring(environment);
+    options = null;
+    const providers = provideMonitoring(options, environment.production);
     expect(providers).toBeNull();
   });
 
   it('should provide monitoring base options', () => {
-    const providers = provideMonitoring(environment);
+    const providers = provideMonitoring(options, environment.production);
     expect(providers).toContain({
       provide: MONITORING_OPTIONS,
       useValue: options
@@ -32,7 +29,7 @@ describe('Provide Monitoring', () => {
   });
 
   it('should provide Sentry monitoring if provider is "sentry"', () => {
-    const providers = provideMonitoring(environment);
+    const providers = provideMonitoring(options, environment.production);
     expect(providers.length).toBeGreaterThan(1);
   });
 });
