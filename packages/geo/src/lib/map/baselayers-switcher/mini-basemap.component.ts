dc394d984f34317e92435aebbc30441c347d3330
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-11-17 14:16:25 -0500

    Revert "fix(geo): build with partial mode"
    This reverts commit 3fd33ecff28f382181ddf1e34b6c047167643813.

--- packages/geo/src/lib/map/baselayers-switcher/mini-basemap.component.ts
+++ packages/geo/src/lib/map/baselayers-switcher/mini-basemap.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import {
   AfterViewInit,
   ApplicationRef,
@@ -13,13 +12,10 @@ import OlView from 'ol/View';
 
 import { Layer, LayerOptions } from '../../layer/shared';
 import { LayerService } from '../../layer/shared/layer.service';
-import { MapBrowserComponent } from '../map-browser';
 import { IgoMap } from '../shared';
 
 @Component({
   selector: 'igo-mini-basemap',
-  standalone: true,
-  imports: [CommonModule, MapBrowserComponent],
   templateUrl: './mini-basemap.component.html',
   styleUrls: ['./mini-basemap.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
