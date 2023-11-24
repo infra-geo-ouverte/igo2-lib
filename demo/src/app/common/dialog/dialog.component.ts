f1452f0627546f073b9e6d5cce8bb8415ccc7a61
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-11-24 17:21:33 -0500

    feat(demo): add a dialog example section

---
+++ demo/src/app/common/dialog/dialog.component.ts
@@ -0,0 +1,158 @@
+import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+import {
+  Choice,
+  ConfirmDialogService,
+  FormDialogService,
+  FormFieldConfig,
+  JsonDialogService,
+  SelectValueDialogService
+} from '@igo2/common';
+
+import { SelectValueDialogType } from 'packages/common/src/lib/select-value-dialog/select-value-dialog.enums';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class AppDialogComponent {
+  constructor(
+    private confirmDialogService: ConfirmDialogService,
+    private selectValueDialogService: SelectValueDialogService,
+    private jsonDialogService: JsonDialogService,
+    private formDialogService: FormDialogService
+  ) {}
+
+  confirm() {
+    this.confirmDialogService
+      .open('Do you want to continue?')
+      .subscribe((r) => {
+        alert(`Your choice is: ${r}`);
+      });
+  }
+  yesno() {
+    this.confirmDialogService
+      .open('Is the sky blue today? ', undefined, true)
+      .subscribe((r) => {
+        alert(`Your choice is: ${r}`);
+      });
+  }
+
+  private select(type: SelectValueDialogType = SelectValueDialogType.Select) {
+    const choices: Choice[] = [
+      { value: '1', title: 'Chocolate' },
+      { value: '2', title: 'Candy' },
+      { value: 3, title: 'Cake' }
+    ];
+
+    this.selectValueDialogService.open(choices, type, true).subscribe((r) => {
+      if (r?.choices) {
+        if (r.choices.length > 1) {
+          alert(`Your choice(s) are: ${r.choices}`);
+        } else {
+          alert(`Your choice is: ${r.choices}`);
+        }
+      }
+    });
+  }
+
+  check() {
+    this.select(SelectValueDialogType.Checkbox);
+  }
+  radio() {
+    this.select(SelectValueDialogType.Radio);
+  }
+
+  json() {
+    this.jsonDialogService.open(
+      'A JSON viewer',
+      {
+        isbn: '123-456-222',
+        author: {
+          lastname: 'Doe',
+          firstname: 'Jane'
+        },
+        editor: {
+          lastname: 'Smith',
+          firstname: 'Jane'
+        },
+        title: 'The Ultimate Database Study Guide',
+        category: ['Non-Fiction', 'Technology']
+      },
+      ['category']
+    );
+  }
+  form() {
+    const fieldConfigs: FormFieldConfig[] = [
+      {
+        name: 'id',
+        title: 'ID',
+        options: {
+          cols: 1,
+          validator: Validators.required
+        }
+      },
+      {
+        name: 'name',
+        title: 'Name',
+        options: {
+          cols: 1,
+          validator: Validators.required
+        }
+      },
+      {
+        name: 'status',
+        title: 'Status',
+        type: 'select',
+        options: {
+          cols: 2
+        },
+        inputs: {
+          choices: [
+            { value: 1, title: 'Single' },
+            { value: 2, title: 'Married' }
+          ]
+        }
+      }
+    ];
+
+    this.formDialogService.open(fieldConfigs).subscribe((data) => {
+      if (data) {
+        alert(JSON.stringify(data));
+      }
+    });
+  }
+
+  email() {
+    const fieldConfigs: FormFieldConfig[] = [
+      {
+        name: 'email',
+        title: 'Email',
+        options: {
+          cols: 2,
+          validator: Validators.compose([Validators.required, Validators.email])
+        }
+      },
+      {
+        name: 'password',
+        title: 'Password',
+        options: {
+          cols: 2,
+          validator: Validators.required
+        },
+        inputs: {
+          isPassword: true
+        }
+      }
+    ];
+
+    this.formDialogService.open(fieldConfigs).subscribe((data) => {
+      if (data) {
+        data.password = '°°°°°°°°°°';
+        alert(JSON.stringify(data));
+      }
+    });
+  }
+}
