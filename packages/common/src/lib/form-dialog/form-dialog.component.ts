2f271935702587288adf35dc86220b19a10d9df2
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2023-12-01 09:43:10 -0500

    chore(common): form dialog data is now mandatory

--- packages/common/src/lib/form-dialog/form-dialog.component.ts
+++ packages/common/src/lib/form-dialog/form-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnDestroy, Optional } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { LanguageService } from '@igo2/core';
@@ -21,7 +21,6 @@ export class FormDialogComponent {
     public languageService: LanguageService,
     public dialogRef: MatDialogRef<FormDialogComponent>,
     private formService: FormService,
-    @Optional()
     @Inject(MAT_DIALOG_DATA)
     public data: FormDialogData
   ) {
