bd022305aa91b2abfc62d2f6f23f62d174287e2d
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-12-20 16:45:27 -0500

    fix(packages): format

--- projects/demo/src/app/common/table/table.module.ts
+++ projects/demo/src/app/common/table/table.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { IgoTableModule } from '@igo2/common';
 
-
 import { AppTableRoutingModule } from './table-routing.module';
 import { AppTableComponent } from './table.component';
 
 @NgModule({
-  imports: [
-    AppTableRoutingModule,
-    IgoTableModule,
-    AppTableComponent
-],
+  imports: [AppTableRoutingModule, IgoTableModule, AppTableComponent],
   exports: [AppTableComponent]
 })
 export class AppTableModule {}
