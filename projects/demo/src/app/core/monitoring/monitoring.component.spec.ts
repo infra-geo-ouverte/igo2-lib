7a7099413ed3af8ab63e4e03e890d8a67e80ab81
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-12-20 16:43:27 -0500

    fix(test)

--- projects/demo/src/app/core/monitoring/monitoring.component.spec.ts
+++ projects/demo/src/app/core/monitoring/monitoring.component.spec.ts
@@ -4,20 +4,20 @@ import { MONITORING_OPTIONS } from '@igo2/core';
 
 import { MOCK_MONITORING_OPTIONS } from 'packages/core/src/lib/monitoring/__mocks__/monitoring-mock';
 
-import { MonitoringComponent } from './monitoring.component';
+import { AppMonitoringComponent } from './monitoring.component';
 
 describe('MonitoringComponent', () => {
-  let component: MonitoringComponent;
-  let fixture: ComponentFixture<MonitoringComponent>;
+  let component: AppMonitoringComponent;
+  let fixture: ComponentFixture<AppMonitoringComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MonitoringComponent],
+      imports: [AppMonitoringComponent],
       providers: [
         { provide: MONITORING_OPTIONS, useValue: MOCK_MONITORING_OPTIONS }
       ]
     });
-    fixture = TestBed.createComponent(MonitoringComponent);
+    fixture = TestBed.createComponent(AppMonitoringComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
