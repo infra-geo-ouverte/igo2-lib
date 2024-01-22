6324b35d124f018d495a1e614304cb6f406afd7f
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2024-01-22 11:59:52 -0500

    wip

--- packages/integration/src/lib/geometry-form/geometry-form.module.ts
+++ packages/integration/src/lib/geometry-form/geometry-form.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 import { IgoFormModule } from '@igo2/common';
 import { IgoLanguageModule, IgoMessageModule } from '@igo2/core';
-import { IgoGeometryModule } from '@igo2/geo';
 
 import { DataIssueReporterToolComponent } from './data-issue-reporter-tool/data-issue-reporter-tool.component';
 
@@ -14,7 +13,6 @@ import { DataIssueReporterToolComponent } from './data-issue-reporter-tool/data-
     MatButtonModule,
     IgoLanguageModule,
     IgoFormModule,
-    IgoGeometryModule,
     IgoMessageModule
   ],
   declarations: [DataIssueReporterToolComponent],
