d3beb4a5dbaaab6e259e8eb6285f7382825bab85
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2024-01-31 11:13:26 -0500

    feat(geo): add to view controller a dragging subject

--- packages/geo/src/lib/map/shared/controllers/view.ts
+++ packages/geo/src/lib/map/shared/controllers/view.ts
@@ -23,6 +23,8 @@ export interface MapViewControllerOptions {
  * Controller to handle map view interactions
  */
 export class MapViewController extends MapController {
+  readonly dragging$: Subject<void> = new Subject();
+
   /**
    * Observable of the current rotation in radians
    */
@@ -133,6 +135,11 @@ export class MapViewController extends MapController {
         ) as EventsKey
       );
     }
+    this.observerKeys.push(
+      this.olMap.on('pointerdrag', (event: OlMapEvent) =>
+        this.dragging$.next()
+      ) as EventsKey
+    );
 
     this.extent$$ = this.extent$
       .pipe(debounceTime(25))
