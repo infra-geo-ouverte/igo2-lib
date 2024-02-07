a70a90dc42d3bb30ea8210b870e93efaad96f418
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2024-02-07 14:57:55 -0500

    wip PackagerManagerTool: igo form

--- packages/integration/src/lib/storage/package-manager-tool/package-manager-tool.component.ts
+++ packages/integration/src/lib/storage/package-manager-tool/package-manager-tool.component.ts
@@ -1,7 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 import { IgoFormModule, ToolComponent } from '@igo2/common';
+import { PackageManagerService } from '@igo2/geo';
+
+import { BehaviorSubject } from 'rxjs';
+
+interface PackageSelectionData {
+  name: string;
+}
 
 @ToolComponent({
   name: 'packageManager',
@@ -16,4 +24,14 @@ import { IgoFormModule, ToolComponent } from '@igo2/common';
   styleUrls: ['./package-manager-tool.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PackageManagerToolComponent {}
+export class PackageManagerToolComponent {
+  form$ = new BehaviorSubject<Form>(undefined);
+
+  data$ = new BehaviorSubject<{ [key: string]: any }>(undefined);
+
+  constructor(private packageManagerService: PackageManagerService) {}
+
+  onSubmit(data: PackageSelectionData): void {
+    console.log('onSubmit');
+  }
+}
