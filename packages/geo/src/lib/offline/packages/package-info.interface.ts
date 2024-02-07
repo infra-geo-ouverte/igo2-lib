bafa5a79ad31ca5b825a9343fa4640e6197d1a2f
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2024-02-07 14:57:28 -0500

    PackageManagerService: fetch package info

---
+++ packages/geo/src/lib/offline/packages/package-info.interface.ts
@@ -0,0 +1,6 @@
+export interface PackageInfo {
+  id: string;
+  title: string;
+  expiration: Date;
+  size: number;
+}
--- packages/geo/src/lib/offline/packages/package-manager.service.ts
+++ packages/geo/src/lib/offline/packages/package-manager.service.ts
@@ -1,10 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { PackageInfo } from './package-info.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackageManagerService {
+  private packagesSubject: BehaviorSubject<PackageInfo[]> = new BehaviorSubject(
+    []
+  );
 
-  constructor() { }
+  get packages$(): Observable<PackageInfo[]> {
+    return this.packagesSubject;
+  }
 
+  get packages() {
+    return this.packagesSubject.value;
+  }
+
+  constructor(private http: HttpClient) {
+    this.actualizePackages();
+  }
+
+  actualizePackages() {
+    this.http
+      .get<PackageInfo[]>('assets/packages/tile-packages.json')
+      .subscribe((newPackages) => {
+        console.log('new packages', newPackages);
+        this.packagesSubject.next(newPackages);
+      });
+  }
 }
