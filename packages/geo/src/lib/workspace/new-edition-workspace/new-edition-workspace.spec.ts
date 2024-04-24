f881459630543e1837ed17c736381cabad05789a
Olivier Saint-Cyr <saintcyr.olivier1@gmail.com>
2024-04-24 15:26:13 -0400

    test(new-edition-workspace): test create feature happy path

---
+++ packages/geo/src/lib/workspace/new-edition-workspace/new-edition-workspace.spec.ts
@@ -0,0 +1,147 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { MessageService } from '@igo2/core/message';
+
+import { DataSourceOptions, FeatureDataSource } from '../../datasource';
+import { FeatureStore } from '../../feature';
+import { GeoWorkspaceOptions, VectorLayer } from '../../layer/shared';
+import { EditionWorkspaceTableTemplateFactory } from './edition-table-template-factory';
+import { EditionFeature, NewEditionWorkspace } from './new-edition-workspace';
+import { createTestIgoMap } from './tests.utils';
+
+describe('NewEditionWorkspace', () => {
+  let workspace: NewEditionWorkspace;
+
+  const editionUrl = 'http://testing.com';
+
+  let http: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  let dialog: MatDialog;
+  let messageService: jasmine.SpyObj<MessageService>;
+  const type = 'Point';
+
+  const dsOptions: DataSourceOptions = {
+    type: 'wfs',
+    edition: {
+      enabled: true,
+      baseUrl: '/',
+      addUrl: '/',
+      modifyUrl: '/',
+      deleteUrl: '/',
+      geomType: type,
+      hasGeometry: true
+    },
+    sourceFields: [
+      {
+        name: 'id',
+        primary: true
+      },
+      {
+        name: 'name'
+      }
+    ]
+  };
+
+  class ImplNewEditionWorkspace extends NewEditionWorkspace {
+    getUpdateBody(feature: EditionFeature): Object {
+      return feature.properties;
+    }
+
+    getCreateBody(feature: EditionFeature): Object {
+      return feature.properties;
+    }
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MatDialogModule]
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    dialog = TestBed.inject(MatDialog);
+    messageService = jasmine.createSpyObj(MessageService, ['success', 'error']);
+
+    const map = createTestIgoMap();
+    const layer = new VectorLayer({
+      source: new FeatureDataSource(dsOptions)
+    });
+
+    map.init();
+    map.addLayer(layer);
+
+    const entityStore = new FeatureStore([], { map });
+    entityStore.bindLayer(layer);
+
+    const templateFactory = new EditionWorkspaceTableTemplateFactory();
+
+    const options = {
+      id: '1',
+      title: 'test-edition',
+      map,
+      layer,
+      editionUrl,
+      entityStore,
+      meta: {
+        tableTemplate: undefined
+      }
+    };
+
+    workspace = new ImplNewEditionWorkspace(
+      http,
+      dialog,
+      messageService,
+      options
+    );
+
+    templateFactory.addTemplateToWorkspace(workspace, layer);
+
+    layer.options.workspace = Object.assign({}, layer.options.workspace, {
+      srcId: layer.id,
+      workspaceId: layer.id,
+      enabled: true
+    } as GeoWorkspaceOptions);
+
+    layer.options.sourceOptions = dsOptions;
+  });
+
+  it('should create the workspace', () => {
+    expect(workspace).toBeTruthy();
+  });
+
+  it('should create a feature', () => {
+    const fieldsValues = { name: 'New Feature' };
+
+    workspace.createFeature();
+    const entities = workspace.entityStore.all();
+    const newFeature = entities[0];
+
+    newFeature.properties = {
+      ...newFeature.properties,
+      ...fieldsValues
+    };
+    workspace.saveFeature(newFeature);
+
+    const req = httpTestingController.expectOne(editionUrl + '/');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(fieldsValues);
+    expect(req.request.url).toEqual(editionUrl + dsOptions.edition.addUrl);
+
+    expect(workspace.isLoading).toBeTruthy();
+
+    req.flush({});
+
+    expect(workspace.isLoading).toBeFalsy();
+
+    expect(messageService.success).toHaveBeenCalledWith(
+      'igo.geo.workspace.addSuccess'
+    );
+  });
+});
