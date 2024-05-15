ca633c1f24cab90e24615609b98e94e9e142f0dd
Aziz <119948730+aziz-access@users.noreply.github.com>
2024-05-15 20:11:58 +0100

    fix(geo): Print set the same horizontal margin for the map (#1662)
    ---------
    
    Co-authored-by: Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>

--- packages/geo/src/lib/directions/shared/directions.service.ts
+++ packages/geo/src/lib/directions/shared/directions.service.ts
@@ -74,14 +74,14 @@ export class DirectionsService {
       format: 'Letter',
       unit: 'mm' // default
     });
-    const dimensions = [
-      doc.internal.pageSize.width,
-      doc.internal.pageSize.height
-    ];
-    const margins = [10, 10, 10, 10];
-    const width = dimensions[0];
-    const height = dimensions[1] - margins[0] - margins[2];
-    const size: [number, number] = [width, height];
+
+    const { width: pageWidth, height: pageHeight } = doc.internal.pageSize;
+
+    /** top | right | bottom | left */
+    const margins: [number, number, number, number] = [20, 10, 20, 10];
+    const width = pageWidth - margins[3] - margins[1];
+    const height = pageHeight - margins[0] - margins[2];
+    const imageDimensions: [number, number] = [width, height];
 
     const title = `${direction.title} (${formatDistance(
       direction.distance
@@ -92,10 +92,16 @@ export class DirectionsService {
       align: 'center'
     });
 
-    margins[0] += 20;
     const resolution = 96; // Default is 96
     this.printService
-      .addMap(doc, map, resolution, size, margins, PrintLegendPosition.none)
+      .addMap(
+        doc,
+        map,
+        resolution,
+        imageDimensions,
+        margins,
+        PrintLegendPosition.none
+      )
       .subscribe(async (status: SubjectStatus) => {
         if (status === SubjectStatus.Done) {
           await this.addInstructions(doc, direction, title);
