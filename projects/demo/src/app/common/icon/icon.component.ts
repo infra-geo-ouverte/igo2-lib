0526198f260f356b86673b47670fd9e47abc1d86
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2024-05-16 13:55:57 -0400

    fix(demo): add doc for icons

--- projects/demo/src/app/common/icon/icon.component.ts
+++ projects/demo/src/app/common/icon/icon.component.ts
@@ -24,12 +24,14 @@ import { ExampleViewerComponent } from '../../components/example/example-viewer/
 })
 export class AppIconComponent {
   exampleIndexHtml: string = EXAMPLE_INDEX_HTML;
+  exampleIndexStaticHtml: string = EXAMPLE_INDEX_STATIC_HTML;
   exampleStyleVariable: string = EXAMPLE_STYLE_VARIABLE;
   exampleProvider: string = EXAMPLE_PROVIDER;
 
   nameControl = new FormControl<string>('shopping_cart_checkout');
 }
 
+const EXAMPLE_INDEX_STATIC_HTML = `<link rel="stylesheet" href=""https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0"" />`;
 const EXAMPLE_INDEX_HTML = `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />`;
 
 const EXAMPLE_STYLE_VARIABLE = `.material-symbols-outlined {
