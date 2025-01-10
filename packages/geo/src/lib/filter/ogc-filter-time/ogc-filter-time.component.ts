3e38819a57529c697d30b8f328236b7e6493e9aa
aziz <aziz@access-international.dev>
2025-01-10 21:52:26 +0100

    fix(geo): fix ogc date format
    (cherry picked from commit 0c02bb63696a06e105c689f1fc9a8b232aaca7e1)

--- packages/geo/src/lib/filter/ogc-filter-time/ogc-filter-time.component.ts
+++ packages/geo/src/lib/filter/ogc-filter-time/ogc-filter-time.component.ts
@@ -232,6 +232,9 @@ export class OgcFilterTimeComponent implements OnInit {
     refreshFilter = true
   ) {
     if (typeof value === 'string') {
+      if (!this.isValidDate(value)) {
+        return;
+      }
       value = new Date(value);
     }
     let valueTmp = this.getDateTime(value, position);
@@ -786,5 +789,13 @@ export class OgcFilterTimeComponent implements OnInit {
     }
     this.setFilterStateDisable();
     this.updateValues();
+  }
+
+  private isValidDate(value: string): boolean {
+    if (/^\d+$/.test(value)) {
+      return false;
+    }
+    const date = new Date(value);
+    return date instanceof Date && !isNaN(date.getTime());
   }
 }
