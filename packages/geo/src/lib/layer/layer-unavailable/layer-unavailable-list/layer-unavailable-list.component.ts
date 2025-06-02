cf77ff9e47343301d7e3bdec3267eb19b9420544
vixisoft <vixisoft@outlook.com>
2025-06-02 16:29:52 -0400

    fix(layer-unavailable-list): fix missing import

--- packages/geo/src/lib/layer/layer-unavailable/layer-unavailable-list/layer-unavailable-list.component.ts
+++ packages/geo/src/lib/layer/layer-unavailable/layer-unavailable-list/layer-unavailable-list.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { ListComponent, ListItemDirective } from '@igo2/common/list';
@@ -11,7 +12,7 @@ import { LayerUnavailableComponent } from '../layer-unavailable.component';
   styleUrls: ['./layer-unavailable-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [ListComponent, ListItemDirective, LayerUnavailableComponent]
+  imports: [NgFor, ListComponent, ListItemDirective, LayerUnavailableComponent]
 })
 export class LayerUnavailableListComponent {
   @Input() layersOptions: AnyLayerOptions[];
