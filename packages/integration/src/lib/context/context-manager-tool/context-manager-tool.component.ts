aa68e277b13463b23e7434bdb53e8ac029ead67e
aziz <aziz@access-international.dev>
2025-11-13 14:49:40 +0100

    fix(integration): context media service

--- packages/integration/src/lib/context/context-manager-tool/context-manager-tool.component.ts
+++ packages/integration/src/lib/context/context-manager-tool/context-manager-tool.component.ts
@@ -1,8 +1,12 @@
 import { Component, inject, input } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { ToolComponent } from '@igo2/common/tool';
 import { ContextListComponent } from '@igo2/context';
+import { Media, MediaService } from '@igo2/core/media';
 import { IgoMap } from '@igo2/geo';
+
+import { map } from 'rxjs';
 
 import { MapState } from '../../map/map.state';
 import { ToolState } from '../../tool/tool.state';
@@ -20,12 +24,17 @@ import { ToolState } from '../../tool/tool.state';
 export class ContextManagerToolComponent {
   private toolState = inject(ToolState);
   private mapState = inject(MapState);
+  private mediaService = inject(MediaService);
 
   readonly toolToOpenOnContextChange = input('mapTools');
 
   get map(): IgoMap {
     return this.mapState.map;
   }
+
+  isDesktop = toSignal(
+    this.mediaService.media$.pipe(map((value) => value === Media.Desktop))
+  );
 
   editContext() {
     this.toolState.toolbox.activateTool('contextEditor');
