0d561a15bc2013bb32836a84f9ed8eedf4c4f7c1
julabed <abednego.jules@mrnf.gouv.qc.ca>
2026-01-19 16:38:57 -0500

    refactor(core): simplify button

--- packages/core/language/src/public_api.ts
+++ packages/core/language/src/public_api.ts
@@ -1,4 +1,3 @@
 export * from './language.module';
 export * from './shared';
 export * from './language-switch/language-switch.component';
-export * from './language-switch/language-switcher-config.service';
