ac2941740312321a6ae98b5edb7414d6acff8cc5
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2026-01-26 11:14:42 -0500

    refactor(style): update getStyle  to return LayerRandomOlStyleFunction when options are undefined

--- packages/geo/src/lib/style/style-service/style.service.ts
+++ packages/geo/src/lib/style/style-service/style.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 
-import { isAnyOlStyle } from '../shared';
+import { isAnyOlStyle, LayerRandomOlStyleFunction } from '../shared';
 import { AnyOlStyle, AnyStyle } from '../shared/layer/layer-style.interface';
 
 @Injectable()
 export class StyleService {
   async getStyle(options: AnyStyle): Promise<AnyOlStyle> {
-    return isAnyOlStyle(options) ? options : undefined;
+    return isAnyOlStyle(options) ? options : LayerRandomOlStyleFunction();
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
