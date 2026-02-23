559f5890a6c368ac5ba9bafb930db9a1c95e7351
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2026-02-23 10:03:24 -0500

    fix(auth): accumulate changes for user preferences request

--- packages/auth/src/lib/shared/auth-storage.service.ts
+++ packages/auth/src/lib/shared/auth-storage.service.ts
@@ -1,20 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, OnDestroy, inject } from '@angular/core';
 
 import { ConfigService } from '@igo2/core/config';
 import { BaseStorage, StorageScope } from '@igo2/core/storage';
+
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 import { AuthStorageOptions } from './auth-storage.interface';
 import { AuthService } from './auth.service';
 import { TokenService } from './token.service';
 
+const DEBOUNCE_TIME = 3000; // 3 seconds;
+
 @Injectable({
   providedIn: 'root'
 })
-export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
+export class AuthStorageService
+  extends BaseStorage<AuthStorageOptions>
+  implements OnDestroy
+{
   private http = inject(HttpClient);
   private authService = inject(AuthService);
   private tokenService = inject(TokenService);
+
+  private preferencesChanged$ = new Subject<void>();
+  private destroy$ = new Subject<void>();
+  private pendingPreferences: Record<string, unknown> = {};
+  private serverPreferences: Record<string, unknown> = {};
 
   constructor() {
     const config = inject(ConfigService);
@@ -23,18 +36,24 @@ export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
 
     this.authService.authenticate$.subscribe((isAuthenticated) => {
       if (isAuthenticated && this.options.url) {
-        this.http
-          .get(this.options.url)
-          .subscribe((userIgo: { preference: object }) => {
-            if (userIgo && userIgo.preference) {
-              for (const key of Object.keys(userIgo.preference)) {
-                const value = userIgo.preference[key];
-                super.set(key, value);
-              }
-            }
-          });
+        this.getUser();
       }
     });
+
+    this.preferencesChanged$
+      .pipe(
+        debounceTime(DEBOUNCE_TIME), // Wait for more changes to accumulate
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => {
+        this.syncPreferences();
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.preferencesChanged$.complete();
   }
 
   set(
@@ -47,9 +66,8 @@ export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
       this.authService.authenticated &&
       this.options.url
     ) {
-      const preference = {};
-      preference[key] = value;
-      this.http.patch(this.options.url, { preference }).subscribe();
+      this.pendingPreferences[key] = value;
+      this.preferencesChanged$.next();
     }
     super.set(key, value, scope);
   }
@@ -60,9 +78,8 @@ export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
       this.authService.authenticated &&
       this.options.url
     ) {
-      const preference = {};
-      preference[key] = undefined;
-      this.http.patch(this.options.url, { preference }).subscribe();
+      this.pendingPreferences[key] = undefined;
+      this.preferencesChanged$.next();
     }
     super.remove(key, scope);
   }
@@ -73,17 +90,8 @@ export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
       this.authService.authenticated &&
       this.options.url
     ) {
-      this.http
-        .patch(
-          this.options.url,
-          { preference: {} },
-          {
-            params: {
-              mergePreference: 'false'
-            }
-          }
-        )
-        .subscribe();
+      this.pendingPreferences = {};
+      this.preferencesChanged$.next();
     }
 
     let token: string;
@@ -96,21 +104,45 @@ export class AuthStorageService extends BaseStorage<AuthStorageOptions> {
     if (token) {
       this.tokenService.set(token);
     }
+  }
 
-    if (
-      scope === StorageScope.LOCAL &&
-      this.authService.authenticated &&
-      this.options.url
-    ) {
-      this.http
-        .get(this.options.url)
-        .subscribe((userIgo: { preference: object }) => {
-          if (userIgo && userIgo.preference) {
-            for (const key of Object.keys(userIgo.preference)) {
-              const value = userIgo.preference[key];
-              super.set(key, value);
-            }
+  private getUser() {
+    this.http
+      .get(this.options.url)
+      .subscribe((userIgo: { preference: object }) => {
+        if (userIgo && userIgo.preference) {
+          this.serverPreferences = { ...userIgo.preference };
+          for (const key of Object.keys(userIgo.preference)) {
+            const value = userIgo.preference[key];
+            super.set(key, value);
           }
+        }
+      });
+  }
+
+  private syncPreferences() {
+    if (Object.keys(this.pendingPreferences).length === 0) {
+      return;
+    }
+
+    // Filter out preferences that haven't actually changed
+    const changedPreferences: Record<string, unknown> = {};
+    for (const key of Object.keys(this.pendingPreferences)) {
+      const newValue = this.pendingPreferences[key];
+      const oldValue = this.serverPreferences[key];
+
+      if (JSON.stringify(newValue) !== JSON.stringify(oldValue)) {
+        changedPreferences[key] = newValue;
+      }
+    }
+
+    this.pendingPreferences = {};
+
+    if (Object.keys(changedPreferences).length > 0) {
+      this.http
+        .patch(this.options.url, { preference: changedPreferences })
+        .subscribe(() => {
+          Object.assign(this.serverPreferences, changedPreferences);
         });
     }
   }
