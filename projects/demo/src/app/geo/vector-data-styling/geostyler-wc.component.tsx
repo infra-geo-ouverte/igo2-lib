6e0a5377eceb818f3a6aac806c55d725bfe0ded6
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-05-12 14:22:30 -0400

    feat: add locale

--- projects/demo/src/app/geo/vector-data-styling/geostyler-wc.component.tsx
+++ projects/demo/src/app/geo/vector-data-styling/geostyler-wc.component.tsx
@@ -1,5 +1,6 @@
 import r2wc from '@r2wc/react-to-web-component';
-import { Style } from 'geostyler';
+import { GeoStylerContext, Style, locale } from 'geostyler';
+import {Data} from 'geostyler-data';
 import React from 'react';
 
 const GEOSTYLER_WEB_COMPONENT_TAG = 'geostyler-style-wc';
@@ -8,7 +9,13 @@ const GeostylerStyleAdapter: React.FC<{
   geostylerStyle: any;
   handleStyleChange: (newStyle: any) => void;
 }> = ({ geostylerStyle, handleStyleChange }) => {
-  return <Style style={geostylerStyle} onStyleChange={handleStyleChange} />;
+
+  
+  return (
+    <GeoStylerContext value={{ locale: locale.fr_FR }}>
+      <Style style={geostylerStyle} onStyleChange={handleStyleChange} />;
+    </GeoStylerContext>
+  );
 };
 
 const GeostylerWebComponent = r2wc(GeostylerStyleAdapter, {
