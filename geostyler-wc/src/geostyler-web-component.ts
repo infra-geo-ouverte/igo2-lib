5c8b9c59d5a32e9afb758ab440fc797c8224ea5f
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-05-22 13:35:32 -0400

    chore: remove `/geostyler-wc` from index

--- geostyler-wc/src/geostyler-web-component.ts
+++
@@ -1,7 +0,0 @@
-import { GeostylerWebComponent } from './GeostylerWebComponent.js';
-
-const GEOSTYLER_WEB_COMPONENT_TAG = 'geostyler-style-wc';
-
-if (!customElements.get(GEOSTYLER_WEB_COMPONENT_TAG)) {
-  customElements.define(GEOSTYLER_WEB_COMPONENT_TAG, GeostylerWebComponent);
-}
--- geostyler-wc/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { GeostylerWebComponent } from './GeostylerWebComponent.js';
