e257242e26e69b38030854681ae50decd7f231c4
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-06-15 16:11:20 -0400

    feat(workspace, edition): define EditionStrategy interface

---
+++ packages/geo/src/lib/workspace/new-edition-workspace/strategy/edition-strategy.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+
+import { Feature } from '../../../feature/shared';
+
+export type EditionVerb = 'PATCH' | 'PUT';
+
+export interface ItemsQuery {
+  bbox?: [number, number, number, number];
+  limit?: number;
+  offset?: number;
+}
+
+export interface EditionStrategy {
+  getItemsUrl(query: ItemsQuery): string;
+  parseItems(response: unknown): Feature[];
+  create(feature: Feature): Observable<string>;
+  update(edit: { feature: Feature; snapshot: Feature }): Observable<void>;
+  delete(feature: Feature): Observable<void>;
+}
