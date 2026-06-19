7597484e7252ff8beb909640e3335ef0b368932c
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-06-19 12:25:21 -0400

    feat: add `EditionTableActions`

---
+++ packages/geo/src/lib/workspace/new-edition-workspace/edition-table-actions.ts
@@ -0,0 +1,29 @@
+import { Feature } from '../../feature';
+import { VectorLayer } from '../../layer';
+import { NewEditionWorkspace } from './new-edition-workspace';
+
+export interface EditionTableActions {
+  onEdit(entity: Feature): void;
+  onDelete(entity: Feature): void;
+  onSave(entity: Feature): void;
+  onCancel(entity: Feature): void;
+  isBusy(): boolean;
+  canModify(): boolean;
+  canDelete(): boolean;
+}
+
+export function createEditionTableActions(
+  workspace: NewEditionWorkspace,
+  layer: VectorLayer
+): EditionTableActions {
+  const edition = layer.dataSource.options.edition;
+  return {
+    onEdit: (e) => workspace.updateFeature(e),
+    onDelete: (e) => workspace.deleteFeature(e),
+    onSave: (e) => workspace.saveFeature(e),
+    onCancel: (e) => workspace.cancelEdit(e),
+    isBusy: () => workspace.isLoading(),
+    canModify: () => edition?.modifyButton !== false,
+    canDelete: () => edition?.deleteButton !== false
+  };
+}
