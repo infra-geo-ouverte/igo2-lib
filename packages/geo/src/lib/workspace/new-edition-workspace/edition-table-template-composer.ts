a9a51ddeafa95f9c4963f083c43359ac1a52cd55
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-06-19 12:29:50 -0400

    feat: add `EntityTableTemplateComposer`

---
+++ packages/geo/src/lib/workspace/new-edition-workspace/edition-table-template-composer.ts
@@ -0,0 +1,133 @@
+import {
+  EntityTableButton,
+  EntityTableColumn,
+  EntityTableColumnRenderer,
+  EntityTableTemplate
+} from 'packages/common/entity/src/shared';
+
+import {
+  RelationOptions,
+  SourceFieldsOptionsParams
+} from '../../datasource/shared';
+import { Feature } from '../../feature/shared';
+import { EditionTableActions } from './edition-table-actions';
+
+export interface TableTemplateInput {
+  fields: SourceFieldsOptionsParams[];
+  relations: RelationOptions[];
+  actions: EditionTableActions;
+}
+
+export class EditionTableTemplateComposer {
+  compose(input: TableTemplateInput): EntityTableTemplate {
+    return {
+      selection: false,
+      sort: true,
+      columns: [
+        ...this.sourceColumns(input.fields),
+        ...this.relationColumns(input.relations),
+        this.actionColumn(input.actions)
+      ]
+    };
+  }
+  private sourceColumns(
+    fields: SourceFieldsOptionsParams[]
+  ): EntityTableColumn[] {
+    // TODO implement domainValues
+    //  if (field.type === 'list' || field.type === 'autocomplete') {
+    return fields.map(
+      (field) =>
+        ({
+          name: `properties.${field.name}`,
+          title: field.alias ? field.alias : field.name,
+          renderer: EntityTableColumnRenderer.UnsanitizedHTML,
+          valueAccessor: undefined,
+          cellClassFunc: () => {
+            const cellClass: Record<string, boolean> = {};
+
+            if (field.type) {
+              cellClass[`class_${field.type}`] = true;
+            }
+
+            return cellClass;
+          },
+          primary: field.primary === true ? true : false,
+          visible: field.visible,
+          validation: field.validation,
+          linkColumnForce: field.linkColumnForce,
+          type: field.type,
+          domainValues: undefined,
+          relation: undefined,
+          multiple: field.multiple,
+          step: field.step,
+          tooltip: field.tooltip
+        }) satisfies EntityTableColumn
+    );
+  }
+
+  private relationColumns(relations: RelationOptions[]): EntityTableColumn[] {
+    return relations.map(
+      (relation: RelationOptions) =>
+        ({
+          name: `properties.${relation.name}`,
+          title: relation.alias ? relation.alias : relation.name,
+          renderer: EntityTableColumnRenderer.Icon,
+          icon: relation.icon,
+          //   parent: relation.parent, // TODO ?
+          type: 'relation',
+          tooltip: relation.tooltip,
+          onClick: () => {
+            throw Error('Not yet implemented');
+            // if (this.adding$.getValue() === false) {
+            //   this.ws$.next(relation.title);
+            // }
+          },
+          cellClassFunc: () => {
+            return { class_icon: true };
+          }
+        }) satisfies EntityTableColumn
+    );
+  }
+
+  private actionColumn(actions: EditionTableActions): EntityTableColumn {
+    return {
+      name: 'edition',
+      title: '',
+      renderer: EntityTableColumnRenderer.ButtonGroup,
+      primary: false,
+      valueAccessor: (entity) => {
+        const feature = entity as Feature;
+        return [
+          {
+            editMode: false,
+            icon: 'edit',
+            color: 'primary',
+            disabled: !actions.canModify(),
+            click: () => actions.onEdit(feature)
+          } satisfies EntityTableButton<Feature>,
+          {
+            editMode: false,
+            icon: 'delete',
+            color: 'warn',
+            disabled: !actions.canDelete(),
+            click: () => actions.onDelete(feature)
+          } satisfies EntityTableButton<Feature>,
+          {
+            editMode: true,
+            icon: 'check',
+            color: 'primary',
+            disabled: actions.isBusy(),
+            click: () => actions.onSave(feature)
+          },
+          {
+            editMode: true,
+            icon: 'close',
+            color: 'primary',
+            disabled: actions.isBusy(),
+            click: () => actions.onCancel(feature)
+          }
+        ] satisfies EntityTableButton<Feature>[];
+      }
+    } satisfies EntityTableColumn;
+  }
+}
