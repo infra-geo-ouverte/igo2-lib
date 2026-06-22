3d59dbeee9a62371fe01217069fd8b71ad6c78d3
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-06-22 13:37:06 -0400

    style: reorder class members for readability

--- packages/geo/src/lib/workspace/shared/edition-workspace.ts
+++ packages/geo/src/lib/workspace/shared/edition-workspace.ts
@@ -9,8 +9,7 @@ import { ConfigService } from '@igo2/core/config';
 import Collection from 'ol/Collection';
 import OlFeature from 'ol/Feature';
 import OlGeoJSON from 'ol/format/GeoJSON';
-import type { Type } from 'ol/geom/Geometry';
-import type { default as OlGeometry } from 'ol/geom/Geometry';
+import type { default as OlGeometry, Type } from 'ol/geom/Geometry';
 import OlModify from 'ol/interaction/Modify';
 import OlVectorSource from 'ol/source/Vector';
 import * as OlStyle from 'ol/style';
@@ -26,39 +25,21 @@ import { ImageLayer, VectorLayer } from '../../layer/shared';
 import { IgoMap } from '../../map/shared/map';
 import { EditionFeature } from './edition-workspace.interface';
 
+interface EditionWorkspaceMeta {
+  tableTemplate: EntityTableTemplate;
+}
+
 interface EditionWorkspaceOptions extends WorkspaceOptions<EditionWorkspaceMeta> {
   layer: ImageLayer | VectorLayer;
   map: IgoMap;
 }
 
-interface EditionWorkspaceMeta {
-  tableTemplate: EntityTableTemplate;
-}
-
 export class EditionWorkspace extends Workspace {
-  readonly inResolutionRange$ = new BehaviorSubject<boolean>(true);
+  public readonly inResolutionRange$ = new BehaviorSubject<boolean>(true);
 
-  get layer(): ImageLayer | VectorLayer {
-    return this.options.layer;
-  }
-
-  get map(): IgoMap {
-    return this.options.map;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore override return type
-  get meta(): EditionWorkspaceMeta | undefined {
-    return this.options.meta ?? undefined;
-  }
-
-  private drawControl: DrawControl;
-  private drawEnd$$!: Subscription;
-  private olDrawingLayerSource = new OlVectorSource();
-  private olDrawingLayer: VectorLayer;
+  public fillColor!: string;
   public geometryType = GeometryType; // Reference to the GeometryType enum
   public modify?: OlModify; // Reference to the ol interaction
-
   public modifyStyle = new OlStyle.Style({
     stroke: new OlStyle.Stroke({
       color: 'rgba(255,255,255,1)',
@@ -78,18 +59,13 @@ export class EditionWorkspace extends Workspace {
       })
     })
   });
-
-  private newFeaturefilterClauseFunc = (record: EntityRecord<object>) => {
-    return record.state.newFeature === true;
-  };
-
-  private editFeaturefilterClauseFunc = (record: EntityRecord<object>) => {
-    return record.state.edit === true;
-  };
-
-  public fillColor!: string;
   public strokeColor!: string;
   public strokeWidth!: number;
+
+  private drawControl: DrawControl;
+  private drawEnd$$!: Subscription;
+  private olDrawingLayer: VectorLayer;
+  private olDrawingLayerSource = new OlVectorSource();
 
   constructor(
     private configService: ConfigService,
@@ -127,31 +103,102 @@ export class EditionWorkspace extends Workspace {
     this.map.layerController.remove(this.olDrawingLayer);
   }
 
-  getDeleteUrl(feature: Feature): string {
-    let url: string | undefined;
-    const baseUrl = this.layer.dataSource.options.edition?.baseUrl;
-    const deleteUrl = this.layer.dataSource.options.edition?.deleteUrl;
-    if (baseUrl?.length) {
-      url = this.configService.getConfig('edition.url')
-        ? this.configService.getConfig('edition.url') +
-          baseUrl +
-          '?' +
-          deleteUrl
-        : baseUrl + '?' + deleteUrl;
-    } else {
-      url = this.configService.getConfig('edition.url')
-        ? this.configService.getConfig('edition.url') + deleteUrl
-        : deleteUrl;
-    }
-
-    const id = this.findFeatureId(feature);
-    if (url) {
-      url += id;
-    }
-    return url ?? '';
+  public get layer(): ImageLayer | VectorLayer {
+    return this.options.layer;
   }
 
-  editFeature(feature: EditionFeature) {
+  public get map(): IgoMap {
+    return this.options.map;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore override return type
+  public get meta(): EditionWorkspaceMeta | undefined {
+    return this.options.meta ?? undefined;
+  }
+
+  /**
+   * Create a Draw Control
+   * @param fillColor the fill color
+   * @param strokeColor the stroke color
+   * @param strokeWidth the stroke width
+   * @returns a Draw Control
+   */
+  public createDrawControl(
+    fillColor?: string,
+    strokeColor?: string,
+    strokeWidth?: number
+  ) {
+    const drawControl = new DrawControl({
+      geometryType: undefined as unknown as string,
+      drawingLayerSource: this.olDrawingLayerSource,
+      drawingLayerStyle: new OlStyle.Style({}),
+      interactionStyle: createInteractionStyle(
+        fillColor,
+        strokeColor,
+        strokeWidth
+      )
+    });
+
+    return drawControl;
+  }
+
+  /**
+   * Create a modify interaction to allow a geometry change one feature at the time (drag and drop)
+   */
+  public createModifyInteraction(
+    olFeature: OlFeature<OlGeometry>,
+    feature: Feature
+  ) {
+    if (this.modify) {
+      this.map.ol.removeInteraction(this.modify);
+    }
+    const olCollection = new Collection([olFeature], { unique: true });
+    this.modify = new OlModify({
+      features: olCollection
+    });
+
+    this.map.ol.addInteraction(this.modify);
+    olCollection.forEach((feature) => {
+      feature.setStyle(this.modifyStyle);
+    });
+
+    this.modify.on('modifyend', (event) => {
+      const olGeometry = event.features.getArray()[0]?.getGeometry();
+      if (olGeometry) {
+        this.addFeatureToStore(feature, olGeometry);
+      }
+    });
+  }
+
+  /**
+   * Deactivate the active draw control
+   */
+  public deactivateDrawControl() {
+    if (!this.drawControl) {
+      return;
+    }
+
+    if (this.drawEnd$$) {
+      this.drawEnd$$.unsubscribe();
+    }
+
+    this.drawControl.setOlMap(undefined);
+  }
+
+  /**
+   * Delete drawings layer and source from the map AND feature from the entity store.
+   * Layer refresh will automatically add the new feature into the store.
+   */
+  public deleteDrawings() {
+    this.map.layerController.remove(this.olDrawingLayer);
+    this.olDrawingLayerSource.clear();
+    if (this.modify) {
+      this.map.ol.removeInteraction(this.modify);
+    }
+  }
+
+  public editFeature(feature: EditionFeature) {
     feature.edition = true;
     const editionOpt = this.layer.dataSource.options.edition;
     const id = this.findFeatureId(feature);
@@ -182,79 +229,49 @@ export class EditionWorkspace extends Workspace {
     }
   }
 
-  private findFeatureId(feature: Feature): string | null {
-    const primaryColumn = this.meta?.tableTemplate?.columns
-      .filter((column) => column.primary)
-      .find((column) => {
-        const key = getColumnKeyWithoutPropertiesTag(column.name);
-        return feature.properties[key] != null;
-      });
-
-    if (!primaryColumn) {
-      return null;
+  public getDeleteUrl(feature: Feature): string {
+    let url: string | undefined;
+    const baseUrl = this.layer.dataSource.options.edition?.baseUrl;
+    const deleteUrl = this.layer.dataSource.options.edition?.deleteUrl;
+    if (baseUrl?.length) {
+      url = this.configService.getConfig('edition.url')
+        ? this.configService.getConfig('edition.url') +
+          baseUrl +
+          '?' +
+          deleteUrl
+        : baseUrl + '?' + deleteUrl;
+    } else {
+      url = this.configService.getConfig('edition.url')
+        ? this.configService.getConfig('edition.url') + deleteUrl
+        : deleteUrl;
     }
 
-    const key = getColumnKeyWithoutPropertiesTag(primaryColumn.name);
-    return feature.properties[key] as string | null;
-  }
-
-  /**
-   * Create a Draw Control
-   * @param fillColor the fill color
-   * @param strokeColor the stroke color
-   * @param strokeWidth the stroke width
-   * @returns a Draw Control
-   */
-  createDrawControl(
-    fillColor?: string,
-    strokeColor?: string,
-    strokeWidth?: number
-  ) {
-    const drawControl = new DrawControl({
-      geometryType: undefined as unknown as string,
-      drawingLayerSource: this.olDrawingLayerSource,
-      drawingLayerStyle: new OlStyle.Style({}),
-      interactionStyle: createInteractionStyle(
-        fillColor,
-        strokeColor,
-        strokeWidth
-      )
-    });
-
-    return drawControl;
+    const id = this.findFeatureId(feature);
+    if (url) {
+      url += id;
+    }
+    return url ?? '';
   }
 
   /**
    * Called when the user selects a new geometry type
    * @param geometryType the geometry type selected by the user
    */
-  onGeometryTypeChange(geometryType: Type, feature: Feature) {
+  public onGeometryTypeChange(geometryType: Type, feature: Feature) {
     this.drawControl.setGeometryType(geometryType);
     this.toggleDrawControl(feature);
   }
 
-  /**
-   * Activate the correct control
-   */
-  private toggleDrawControl(feature: Feature) {
-    this.deactivateDrawControl();
-    this.activateDrawControl(feature);
-  }
-
-  /**
-   * Deactivate the active draw control
-   */
-  public deactivateDrawControl() {
-    if (!this.drawControl) {
-      return;
-    }
-
-    if (this.drawEnd$$) {
-      this.drawEnd$$.unsubscribe();
-    }
-
-    this.drawControl.setOlMap(undefined);
-  }
+  private readonly editFeaturefilterClauseFunc = (
+    record: EntityRecord<object>
+  ) => {
+    return record.state.edit === true;
+  };
+  private readonly newFeaturefilterClauseFunc = (
+    record: EntityRecord<object>
+  ) => {
+    return record.state.newFeature === true;
+  };
 
   /**
    * Activate a given control
@@ -307,40 +324,27 @@ export class EditionWorkspace extends Workspace {
     }
   }
 
-  /**
-   * Delete drawings layer and source from the map AND feature from the entity store.
-   * Layer refresh will automatically add the new feature into the store.
-   */
-  deleteDrawings() {
-    this.map.layerController.remove(this.olDrawingLayer);
-    this.olDrawingLayerSource.clear();
-    if (this.modify) {
-      this.map.ol.removeInteraction(this.modify);
+  private findFeatureId(feature: Feature): string | null {
+    const primaryColumn = this.meta?.tableTemplate?.columns
+      .filter((column) => column.primary)
+      .find((column) => {
+        const key = getColumnKeyWithoutPropertiesTag(column.name);
+        return feature.properties[key] != null;
+      });
+
+    if (!primaryColumn) {
+      return null;
     }
+
+    const key = getColumnKeyWithoutPropertiesTag(primaryColumn.name);
+    return feature.properties[key] as string | null;
   }
 
   /**
-   * Create a modify interaction to allow a geometry change one feature at the time (drag and drop)
+   * Activate the correct control
    */
-  createModifyInteraction(olFeature: OlFeature<OlGeometry>, feature: Feature) {
-    if (this.modify) {
-      this.map.ol.removeInteraction(this.modify);
-    }
-    const olCollection = new Collection([olFeature], { unique: true });
-    this.modify = new OlModify({
-      features: olCollection
-    });
-
-    this.map.ol.addInteraction(this.modify);
-    olCollection.forEach((feature) => {
-      feature.setStyle(this.modifyStyle);
-    });
-
-    this.modify.on('modifyend', (event) => {
-      const olGeometry = event.features.getArray()[0]?.getGeometry();
-      if (olGeometry) {
-        this.addFeatureToStore(feature, olGeometry);
-      }
-    });
+  private toggleDrawControl(feature: Feature) {
+    this.deactivateDrawControl();
+    this.activateDrawControl(feature);
   }
 }
