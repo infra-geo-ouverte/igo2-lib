d3948383bdf69d8301dff2917717bb17ae9721ac
Maxime Lamer <83651899+LAMM26@users.noreply.github.com>
2026-09-25 10:53:43 -0400

    fix(geo): fetch all workspace columns
    * fix(geo): fetch all workspace columns
    
    (cherry picked from commit 44898161eb978c677e4ebcb4b5843e77e568ef4f)
    
    * fix: tests

---
+++ packages/geo/src/lib/workspace/shared/workspace.utils.spec.ts
@@ -0,0 +1,37 @@
+import OlFeature from 'ol/Feature';
+
+import { Feature } from '../../feature/shared/feature.interfaces';
+import { getFeatureTableColumns } from './workspace.utils';
+
+describe('getFeatureTableColumns', () => {
+  it('collects columns from every feature', () => {
+    const features: Feature[] = [
+      {
+        type: 'Feature',
+        properties: {},
+        ol: new OlFeature({
+          firstAttribute: 'first',
+          sharedAttribute: 'shared',
+          _internal: 'hidden',
+          boundedBy: 'hidden'
+        })
+      },
+      {
+        type: 'Feature',
+        properties: {},
+        ol: new OlFeature({
+          sharedAttribute: 'shared',
+          secondAttribute: 'second'
+        })
+      }
+    ];
+
+    const columns = getFeatureTableColumns(features);
+
+    expect(columns.map((column) => column.name)).toEqual([
+      'properties.firstAttribute',
+      'properties.sharedAttribute',
+      'properties.secondAttribute'
+    ]);
+  });
+});
